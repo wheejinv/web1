c5c385d41d70ac6d65128be1dff20d4b18413143
whee <wheejinv@naver.com>
2021-12-22 19:34:56 +0900

    webpack build mode - development or production

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path')
 
-module.exports = {
-	entry: './src/index.js',
-	mode: "development",
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
+module.exports = (env) => {
+	console.log(env);
+
+	let mode = env.production ? "production" : "development";
+
+	return {
+		entry: './src/index.js',
+		mode,
+		// mode: "production",
+		output: {
+			filename: 'main.js',
+			path: path.resolve(__dirname, 'dist'),
+		}
 	}
 }
