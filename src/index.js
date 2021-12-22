254d4a777a445574f80b92fb13ad5de7a295dd5e
whee <wheejinv@naver.com>
2021-12-22 20:18:42 +0900

    1. css 확장자에 대해 css-loader, style-loader 적용 2. index.html 에서 css 로드 구문을 빼고 index.js 에서 임포트

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import hello_word from "./Hello.js";
 import world_word from "./World.js";
+import "../css/style.css"
 
 let test = function() {
 	console.log("clicked, ", hello_word, world_word);
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,17 @@ module.exports = (env) => {
 		output: {
 			filename: 'main.js',
 			path: path.resolve(__dirname, 'dist'),
+		},
+		module: {
+			rules: [
+				{
+					test: /\.css$/i, // 확장자가 css 인 코드
+					use: [
+						'style-loader',
+						'css-loader', // 순서상 뒤에 로더가 먼저 실행됨.
+					],
+				}
+			],
 		}
 	}
 }
