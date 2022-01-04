52146c15917ad60e53bf73c2a8ffb423f10d1c77
whee <wheejinv@naver.com>
2022-01-04 14:02:37 +0900

    섹션 11: The Document Object Model (DOM)

---
+++ public/webbootcamp2022/dom/index.js
@@ -0,0 +1,7 @@
+document.querySelector("h1").classList.add("huge");
+document.querySelectorAll('li')[1].classList.add("invisible")
+document.querySelector('button').style.backgroundColor = "yellow";
+document.querySelector('li a').style.color = "red";
+document.querySelectorAll('ol li')[1].innerText = "query Select all";
+
+document.querySelector('a').setAttribute('href', 'https://www.google.com');
