56e3ba26de7ea2b366245359374a7584476c6292
whee <wheejinv@naver.com>
2022-01-07 01:35:16 +0900

    섹션 15: #2 Boss Level Challenge 2 - The Simon Game

--- public/webbootcamp2022/simon/game.js
+++ public/webbootcamp2022/simon/game.js
@@ -1,9 +1,16 @@
 let gamePattern = [];
-let buttonColours = ['red', 'blue', 'green', 'yellow'];
+const buttonColours = ['red', 'blue', 'green', 'yellow'];
+let isPlaying = false;
+let currentLevel = 0;
+let level = 0;
+let isFirstGame = true;
 
 let nextSequence = () => {
 	let randomNumber = Math.floor(Math.random() * 4);
 	let randomChosenColour = buttonColours[randomNumber];
+	level += 1;
+
+	setHead(`Level ${level}`);
 
 	gamePattern.push(randomChosenColour);
 
@@ -17,15 +24,81 @@ let nextSequence = () => {
 		}, 50);
 };
 
+let checkAnswer = (userChosenColour) => {
+	if (gamePattern[currentLevel] === userChosenColour) {
+		currentLevel++;
+
+		if (level === currentLevel) {
+			currentLevel = 0;
+			setTimeout( () => {
+				nextSequence();
+			}, 1000)
+		}
+	} else {
+		gameOver();
+	}
+}
+
+let gameOver = () => {
+	$("body").addClass("game-over");
+
+	setTimeout( () => {
+		$("body").removeClass("game-over");
+	}, 200);
+
+	playSound("wrong");
+
+	isPlaying = false;
+	currentLevel = 0;
+	level = 0;
+	gamePattern = [];
+
+	setHead("Game Over, Press Any Key to Restart");
+}
+
 let playSound = (fileName) => {
 	var audio = new Audio(`./sounds/${fileName}.mp3`);
 	audio.play();
 };
 
+let animatePress = (btn) => {
+		btn.classList.add("pressed");
+
+		setTimeout( () => {
+			btn.classList.remove("pressed");
+		}, 100);
+};
+
+let setHead = (text) => {
+	$("h1").text(text);
+};
+
+$(".btn").on("click", function(event) {
+	if (isPlaying === false) {
+		return;
+	}
+
+	console.log("ad");
+	playSound(this.id);
+	animatePress(this);
+	checkAnswer(this.id);
+});
+
 $(document).keypress((event) => {
-	if (event.key === "a") {
+	if (isFirstGame) {
+		if (event.key === "a" && isPlaying === false) {
+			isPlaying = true;
+			level = 0;
+			isFirstGame = false;
+			nextSequence();
+		}
+	} else if (isPlaying === false) {
+		isPlaying = true;
+		level = 0;
+		isFirstGame = false;
 		nextSequence();
 	}
-})
+});
+
 
 
