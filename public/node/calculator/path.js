17562b62bb7beec8ef32c685300d5539aaba47ed
whee <wheejinv@naver.com>
2022-01-10 02:17:30 +0900

    섹션 19: Express.js with Node.js #3 루트 요청의 응답으로 index.html 파일을 응답으로 보내기

---
+++ public/node/calculator/path.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+// /Users/whee/dev/private/web1/public/node/calculator/console/assd
+console.log(path.resolve(__dirname + '//console//assd/'));
+
+// /Users/whee/dev/private/web1/public/node/calculator
+console.log(__dirname);
+
