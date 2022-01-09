9c2d82911af8ecdba75327e36e22eed5e4acd2d8
whee <wheejinv@naver.com>
2022-01-09 21:54:45 +0900

    섹션 19: Express.js with Node.js #2 몇개의 라우터 추가. 노드몬으로 server.js 실행 / https://nodemon.io/ $ nodemon server.js

--- public/node/my-express-server/server.js
+++ public/node/my-express-server/server.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// 첫번째 파라미터는 라우터
 app.get('/', (req, res) => {
-	res.send("<h1>Hello, World</h1>");
+	res.send("<h1>Hello, World..</h1>");
+});
+
+app.get('/contact', (req, res) => {
+	res.send("Contact me at: heejin");
+});
+
+app.get('/about', (req, res) => {
+	res.send('About Page');
+});
+
+app.get('/hobbies', (req, res) => {
+	res.send('Hobbie page');
 });
 
 app.listen(port, () => {
