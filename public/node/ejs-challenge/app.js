556936154215c3d1e24a2094e0b80785878c3f8c
wheejinv <wheejinv@naver.com>
2022-01-16 00:26:46 +0900

    섹션 23: Boss Level Challenge 3 - Blog Website #2 1 ~ 9 challenge

--- public/node/ejs-challenge/app.js
+++ public/node/ejs-challenge/app.js
@@ -15,18 +15,32 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get('/', (req, res) => {
+	res.render('home', {
+		startingContent: homeStartingContent,
+	});
+});
 
+app.get('/about', (req, res) => {
+	res.render('about', {
+		aboutContent,
+	})
+})
 
+app.get('/contact', (req, res) => {
+	res.render('contact', {
+		contactContent,
+	})
+})
 
+app.get('/compose', (req, res) => {
+	res.render('compose');
+})
 
-
-
-
-
-
-
-
-
+app.post('/compose', (req, res) => {
+	let {composeText} = req.body;
+	console.log(composeText)
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
