2ac403690d8b242f3c8a20650a7266569286368a
wheejinv <wheejinv@naver.com>
2022-01-15 15:41:44 +0900

    섹션 22: #2 EJS - 제공된 css 적용 - ejs를 템플릿화 작업(두가지 get 요청에 대한 처리 가능: '/', '/work') - submit 버튼에 value 값으로 서버에서 온 값을 다시 보내줘서 서버에서 분기처리 가능하도록

--- public/webbootcamp2022/todolist-v1/app.js
+++ public/webbootcamp2022/todolist-v1/app.js
@@ -7,16 +7,48 @@ const app = express();
 // 렌더에 참조하는 폴더 위치는 views 폴더로 알아서 고정됨.
 app.set('view engine', 'ejs');
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+
+
 app.get("/", (req, res) => {
-
 	var today = new Date();
-	var currentDay = today.getDay();
 
-	var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	let options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+	}
+
+	// https://www.w3schools.com/jsref/jsref_tolocalestring.asp
+	let day = today.toLocaleDateString("en-US", options);
 
 	res.render('list', {
-		kindOfDay: days[currentDay],
+		listTitle: day,
+		newListItems: items,
 	});
+});
+
+app.get("/work", (req, res) => {
+	res.render("list", {
+		listTitle: "Work",
+		newListItems: workItems,
+	})
+});
+
+app.post("/", (req, res) => {
+	let item = req.body.newItem;
+
+	if (req.body.list == "Work") {
+		workItems.push(item);
+		res.redirect("/work");
+	} else {
+		items.push(item);
+		res.redirect("/");
+	}
 });
 
 app.listen(3000, () => {
