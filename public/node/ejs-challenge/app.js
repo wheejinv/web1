423eab4e8c7218973d0dbdd2e2b47b87236151f7
wheejinv <wheejinv@naver.com>
2022-01-16 21:03:22 +0900

    섹션 23: Boss Level Challenge 3 - Blog Website #4 18~21 마무으리. - 해맸던 부분: Array.slice() 는 객체의 경우에는 같은 객체를 반환하는 얕은 복사를 수행함. lodash 라이브러리 깊은 복사 함수를 사용함.

--- public/node/ejs-challenge/app.js
+++ public/node/ejs-challenge/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const toSlug = require(__dirname + '/convertToSlug.js');
+const _ = require("lodash");
 
 const startingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -19,9 +20,19 @@ app.use(express.static("public"));
 let posts = [];
 
 app.get('/', (req, res) => {
+	let clonedPost = _.cloneDeep(posts);
+
 	res.render('home', {
 		startingContent,
-		posts
+		posts: clonedPost.slice(0,3).map(post => {
+			if (post.postBody.length > 100) {
+				post.postBody = post.postBody.slice(0, 100) + ' ...';
+			}
+
+			post.postUrl = '/posts/' + toSlug(post.postTitle);
+
+			return post;
+		})
 	});
 });
 
@@ -41,8 +52,8 @@ app.get('/compose', (req, res) => {
 	res.render('compose');
 })
 
-app.get('/posts/:topic', (req, res) => {
-	let url = req.params.topic.toLowerCase();
+app.get('/posts/:postName', (req, res) => {
+	let url = req.params.postName.toLowerCase();
 
 	let targetPostIndex = posts.findIndex( (post) => {
 		if (url === toSlug(post.postTitle)) {
@@ -58,7 +69,7 @@ app.get('/posts/:topic', (req, res) => {
 			postBody,
 		})
 	} else {
-		res.send('not found topics: ' + req.params.topic);
+		res.send('not found topics: ' + req.params.postName);
 	}
 });
 
