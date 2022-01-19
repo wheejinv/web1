0347f49b46d5b80dc7dd39fe2d77a9d8062c66be
wheejinv <wheejinv@naver.com>
2022-01-19 10:06:21 +0900

    섹션 28: Putting Everything Together #2 todolist-v2, 몽고디비의 데이터베이스인 todolistDB의 items 컬렉션의 값들을 조회(find) 한 결과값을 get 요청인 '/' 에서 보여줌. 생각해볼점: 클라에 아이템 정보를 통으로 넘겨주는게 바람직할까? 그럴 가능성이 높음. 혹은 서버 단에서 한번 재가공해서 줘도 될거고

--- public/node/todolist-v2/app.js
+++ public/node/todolist-v2/app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -10,11 +11,52 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
 
+// mongodb, mongoose
+const itemSchema = new mongoose.Schema({
+	name: String
+});
+const Item = mongoose.model("Item", itemSchema);
+
+let mongoConfig = require('./mongo_config.json');
+const url = `mongodb://${mongoConfig.id}:${mongoConfig.pass}@${mongoConfig.url}/${mongoConfig.dbName}`;
+mongoose.connect(url, () => {
+	console.log('success connect');
+
+	// set default items
+	// const item1 = new Item({
+	// 	name: "Welcome to you todolist!"
+	// });
+	// const item2 = new Item({
+	// 	name: "Hit the + button to off a new item."
+	// });
+	// const item3 = new Item({
+	// 	name: "<-- Hit this to delete an item.>"
+	// });
+	// const defaultItems = [item1, item2, item3];
+	// Item.insertMany(defaultItems, (err) => {
+	// 	if(err) {
+	// 		console.log(err)
+	// 	} else {
+	// 		console.log("insert success");
+	// 	}
+	// })
+});
+
 app.get("/", function(req, res) {
-  res.render("list", {listTitle: "Today", newListItems: items});
+	Item.find({}, (err, docs) => {
+		if(err) {
+			console.log(err);
+			// todo 에러 처리는 어떻게 하는지..? 화면에 보여줄 요소를 그려야 하는데?
+		} else {
+			console.log(docs);
+			res.render("list", {
+				listTitle: "Today",
+				newListItems: docs
+			});
+		}
+	});
 });
 
 app.post("/", function(req, res){
