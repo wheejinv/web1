fab4c1fb21a9faf203b8a23b130f8b40e8a219f5
wheejinv <wheejinv@naver.com>
2022-01-20 00:42:54 +0900

    섹션 28: Putting Everything Together #3 - input 태그에서 hidden type 으로 form 데이터 전송 시 추가 데이터 보내기. - $pull 오퍼레이터로 list 아이템의 items 에서 특정 item 만 추출함. - app.get("/:customListName, ..) Route parameters 처리로 인해 코드가 많이 복잡해짐. 근데 볼거 많음.

--- public/node/todolist-v2/app.js
+++ public/node/todolist-v2/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const _ = require('lodash');
 
 const app = express();
 
@@ -19,29 +20,29 @@ const itemSchema = new mongoose.Schema({
 });
 const Item = mongoose.model("Item", itemSchema);
 
+// set default items
+const item1 = new Item({
+	name: "Welcome to you todolist!"
+});
+const item2 = new Item({
+	name: "Hit the + button to off a new item."
+});
+const item3 = new Item({
+	name: "<-- Hit this to delete an item.>"
+});
+const defaultItems = [item1, item2, item3];
+
+const listSchema = new mongoose.Schema({
+	name: String,
+	items: [itemSchema]
+});
+
+const List = mongoose.model("List", listSchema);
+
 let mongoConfig = require('./mongo_config.json');
 const url = `mongodb://${mongoConfig.id}:${mongoConfig.pass}@${mongoConfig.url}/${mongoConfig.dbName}`;
 mongoose.connect(url, () => {
 	console.log('success connect');
-
-	// set default items
-	// const item1 = new Item({
-	// 	name: "Welcome to you todolist!"
-	// });
-	// const item2 = new Item({
-	// 	name: "Hit the + button to off a new item."
-	// });
-	// const item3 = new Item({
-	// 	name: "<-- Hit this to delete an item.>"
-	// });
-	// const defaultItems = [item1, item2, item3];
-	// Item.insertMany(defaultItems, (err) => {
-	// 	if(err) {
-	// 		console.log(err)
-	// 	} else {
-	// 		console.log("insert success");
-	// 	}
-	// })
 });
 
 app.get("/", function(req, res) {
@@ -50,30 +51,113 @@ app.get("/", function(req, res) {
 			console.log(err);
 			// todo 에러 처리는 어떻게 하는지..? 화면에 보여줄 요소를 그려야 하는데?
 		} else {
-			console.log(docs);
-			res.render("list", {
-				listTitle: "Today",
-				newListItems: docs
-			});
+			// 아이템이 없는 경우 만들어서 다시 '/' 로 리다이렉트
+			if (docs.length === 0) {
+				Item.insertMany(defaultItems, (err) => {
+					if(err) {
+						console.log(err)
+					} else {
+						console.log("insert success");
+					}
+				});
+				res.redirect("/");
+			} else {
+				// console.log(docs);
+				res.render("list", {
+					listTitle: "Today",
+					newListItems: docs
+				});
+			}
 		}
 	});
 });
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
+	const listName = req.body.list;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+	const item = new Item({
+		name: itemName
+	});
+
+	// root 요청인 경우 '/'
+	if (listName === "Today") {
+		item.save();
+		res.redirect("/");
+	} else {
+		// 아닌 경우 해당 아이템을 찾아서 items 에 item 을 넣어주고 저장 후 다시 리다이렉트
+		List.findOne({name: listName}, function(err, foundList) {
+			foundList.items.push(item);
+			foundList.save().then( () =>{
+				res.redirect("/" + listName);
+			})
+		})
+	}
+
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.post('/delete', function(req, res) {
+	let checkedItemID = req.body.checkbox;
+	const listName = req.body.listName;
+
+	if (listName === "Today") {
+		Item.deleteOne({_id: checkedItemID}, (err) => {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log('delete success');
+				res.redirect("/");
+			}
+		})
+	} else {
+		// 배열에서 특정한 얘만 제거 하는 방법
+		List.findOneAndUpdate({
+			name: listName
+		}, {
+			// $pull: https://docs.mongodb.com/manual/reference/operator/update/pull/
+			$pull: {
+				items: {
+					_id: checkedItemID
+				}
+			}
+		}, function(err, results) {
+			if(!err) {
+				res.redirect("/" + listName);
+			}
+		})
+	}
+});
+
+app.get("/:customListName", function(req,res){
+	let todoName = req.params.customListName;
+	todoName = _.capitalize(todoName);
+
+	// 잘못된 크롬 플러그인 때문에 방어 코드. cxMouse
+	if (todoName === 'Null') {
+		return;
+	}
+
+	List.findOne({
+		name: todoName
+	}, function (err, results) {
+		if (err) {
+			console.log(err)
+		} else if (results) {
+			console.log(`results: ${results}`)
+
+			res.render("list", {listTitle: todoName, newListItems: results.items});
+		} else {
+			const list = new List({
+				name: todoName,
+				items: defaultItems
+			})
+
+			list.save().then( () => {
+				res.redirect(`/${todoName}`);
+			})
+		}
+	})
 });
 
 app.get("/about", function(req, res){
