eea94037baad3a3e0f9dde07edbb16ffbe73f6f9
wheejinv <wheejinv@naver.com>
2022-01-24 09:52:16 +0900

    섹션 32: Authentication & Security Level 6 - Google OAuth 2.0 Authentication, 그리고 마무리 passport-google-oauth20 를 활용하여 구글 인증 진행 mongoose-findorcreate: passport-google-oauth20 에서 샘플로 나온 코드를 몽구스에서 가능하도록 만들어주는 플러그인
    사전 작업: 구글 개발자 콘솔에서 웹 어플리케이션 앱 설정하고 oauth 관련 설정을 하였음.
    
    /secrets 요청에서는 로그인된 사용자들의 비밀을 렌더링함.

--- public/node/secrets/app.js
+++ public/node/secrets/app.js
@@ -12,6 +12,8 @@ const app = express();
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const mongoose = require('mongoose');
 // const encrypt = require('mongoose-encryption');
@@ -45,13 +47,16 @@ mongoose.connect(url, (err) => {
 
 const userSchema = new mongoose.Schema({
 	email: String,
-	password: String
+	password: String,
+	googleId: String,
+	secret: String,
 });
 
 // 비밀번호를 해시 및 솔트화하고 저장하는데 사용함.
 // passportLocalMongoose에게 어떤 field를 username을 email로 하겠다
 // https://studyingych.tistory.com/45
 userSchema.plugin(passportLocalMongoose, {usernameField: 'email'});
+userSchema.plugin(findOrCreate);
 
 // encryptionKey, signingKey 생성 방식 SOME_32BYTE_BASE64_STRING, 64: openssl rand -base64 32, openssl rand -base64 32;
 // secret: 아무말이나 대충 쓴 string
@@ -66,8 +71,26 @@ const User = new mongoose.model('User', userSchema);
 // CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+	done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+	User.findById(id, function(err, user) {
+		done(err, user);
+	})
+});
+
+passport.use(new GoogleStrategy({
+	clientID: env.CLIENT_ID,
+	clientSecret: env.CLIENT_SECRET,
+	callbackURL: "http://localhost:3000/auth/google/secrets",
+	userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+}, function (accessToken, refreshToken, profile, cb) {
+	console.log(`profile: ${JSON.stringify(profile)}`)
+	User.findOrCreate({googleId: profile.id}, function (err, user) {
+		return cb(err, user)
+	})
+}))
 
 app.get('/', function(req, res) {
 	res.render('home');
@@ -78,12 +101,61 @@ app.get('/login', function(req, res) {
 });
 
 app.get('/secrets', function(req, res) {
+	// ne: not equall
+	User.find({"secret": {$ne: null}}, function(err, foundUsers) {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log(`foundUsers: ${foundUsers}`);
+			if (foundUsers) {
+				res.render("secrets", {
+					usersWithSecrets: foundUsers
+				})
+			}
+		}
+	})
+});
+
+app.get('/submit', function(req, res) {
 	if (req.isAuthenticated()) {
-		res.render('secrets')
+		res.render('submit')
 	} else {
 		res.redirect('/login');
 	}
 });
+
+app.post('/submit', function(req, res) {
+	const submittedSecret = req.body.secret;
+
+	// 현재 유저를 찾아서 secret 을 저장한다.
+	// 이메일 패스워드 방식에서는 알 수 있음. 구글 로그인은 안나옴.
+	console.log(req.user);
+
+	// req.user.id: 세션에 저장되어 있는 정보임.
+	User.findById(req.user.id, function(err, foundUser) {
+		if (err) {
+			console.log(err)
+		} else {
+			if (foundUser) {
+				foundUser.secret = submittedSecret;
+				foundUser.save().then( () => {
+					res.redirect('/secrets')
+				})
+			}
+		}
+	})
+});
+
+app.get('/auth/google',
+	passport.authenticate('google', {scope: ['profile']})
+);
+
+app.get('/auth/google/secrets',
+	passport.authenticate('google', {failureRedirect: '/login'}),
+	function (req, res) {
+		res.redirect('/secrets')
+	}
+);
 
 app.get('/logout', function(req, res) {
 	req.logout();
