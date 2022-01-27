e138bd769a8534e25a71d8c5c31ee436fa4bc014
wheejinv <wheejinv@naver.com>
2022-01-27 15:30:44 +0900

    update jenkins

--- jenkins_web1.groovy
+++ jenkins_web1.groovy
@@ -2,16 +2,23 @@ pipeline {
     environment {
         BUILD_ENV = "dev"
         DIST_ROOT = "/var/jenkins_home/workspace/httpd_data" // 웹 페이지 결과
+				WORK_ROOT = "/var/jenkins_home/workspace/web1" // 젠킨스에서 git 소스 pull 받아서 보관하고 있는 경로
     }
 
     agent any
 
     stages {
-        stage('SETUP') {
+        stage('COPY SOURCE') {
             steps {
                 script {
                     sh """
-                        echo setup
+												npm run build_prod
+
+                        echo copy public folder
+                        cp -r ${WORK_ROOT}/public/ ${DIST_ROOT}
+
+												echo copy dist folder
+												cp -r ${WORK_ROOT}/dist ${DIST_ROOT}
                     """
                 }
             }
