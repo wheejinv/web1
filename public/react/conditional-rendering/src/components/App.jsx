def3e2283616f9a2c0192f276e8013af002b1c05
wheejinv <wheejinv@naver.com>
2022-01-31 01:52:34 +0900

    섹션 33: React.js - 417. React Conditional Rendering with the Ternary Operator & AND Operator #2 함수로 조건부 렌더링 처리. 우아한 방법은 아님

--- public/react/conditional-rendering/src/components/App.jsx
+++ public/react/conditional-rendering/src/components/App.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 
+let isLoggedIn = false;
+
+function renderConditionally() {
+	if (isLoggedIn) {
+		return <h1>Hello</h1>;
+	} else {
+		return <form className="form">
+			<input type="text" placeholder="Username"/>
+			<input type="password" placeholder="Password"/>
+			<button type="submit">Login</button>
+		</form>;
+	}
+}
+
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+			{renderConditionally()}
     </div>
   );
 }
