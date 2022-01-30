a9360402f94e90da5961bebc8a01b5c30fec0fa7
wheejinv <wheejinv@naver.com>
2022-01-31 01:00:49 +0900

    섹션 33: React.js - 416. Keeper App Project - Part 2 #2 complete

--- public/react/keeper-app-part2/src/components/Note.js
+++ public/react/keeper-app-part2/src/components/Note.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-function Note() {
+function Note(props) {
+	const {title, content} = props;
+
   return (
     <div className="note">
-      <h1>This is the note title</h1>
-      <p>This is the note content</p>
+      <h1>{title}</h1>
+      <p>{content}</p>
     </div>
   );
 }
