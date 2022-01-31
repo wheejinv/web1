39a27f50951642876186d0a6c03c75d5585efa2f
wheejinv <wheejinv@naver.com>
2022-01-31 13:10:22 +0900

    섹션 33: React.js - 421. useState Hook Practice #2 complete practice using {useState, useEffect}

--- public/react/usestate-hook-practice/src/components/App.jsx
+++ public/react/usestate-hook-practice/src/components/App.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
-    </div>
-  );
+	const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+	function updateTime() {
+		setTime(new Date().toLocaleTimeString());
+	}
+
+	useEffect(() => {
+		let timeoutID = setInterval(() => {
+			updateTime();
+		}, 1000);
+
+		return () => {
+			clearInterval(timeoutID);
+		};
+	});
+
+	return (
+		<div className="container">
+			<h1>{time}</h1>
+			<button onClick={updateTime}>Get Time</button>
+		</div>
+	);
 }
 
 export default App;
