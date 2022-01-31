0eba353da0d1bb2a4e9f7d36a814bdf2a47eba2b
wheejinv <wheejinv@naver.com>
2022-01-31 13:50:32 +0900

    섹션 33: React.js - 423. Javascript ES6 Destructuring Challenge Solution #2 complete / 객체 안의 배열 or 객체를 원하는 변수로 Destructuring

--- public/react/es6-destructuring-challenge/src/index.js
+++ public/react/es6-destructuring-challenge/src/index.js
@@ -1,6 +1,28 @@
 // CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
 import ReactDOM from "react-dom";
+import practice from "./practice";
+
+const [tesla, honda] = practice;
+
+// 아래와 동치.
+// const teslaTopColour = tesla.coloursByPopularity[0]
+// const [teslaTopColour] = tesla.coloursByPopularity;
+const {coloursByPopularity: [teslaTopColour]} = tesla;
+
+// const teslaTopSpeed = tesla.speedStats.topSpeed
+const {
+	speedStats: {topSpeed: teslaTopSpeed}
+} = tesla;
+
+// const teslaTopColour = tesla.coloursByPopularity[0]
+// const [hondaTopColour] = honda.coloursByPopularity;
+const {coloursByPopularity: [hondaTopColour]} = honda;
+
+// const hondaTopSpeed = honda.speedStats.topSpeed
+const {
+	speedStats: {topSpeed: hondaTopSpeed}
+} = honda;
 
 ReactDOM.render(
   <table>
