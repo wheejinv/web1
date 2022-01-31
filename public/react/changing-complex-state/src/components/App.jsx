542ffabfdc4b1afdb21d814dcbc162d4239c5b30
wheejinv <wheejinv@naver.com>
2022-02-01 01:13:19 +0900

    섹션 33: React.js - 427. Changing Complex State #3 useState 로 객체 상태 관리하기.

--- public/react/changing-complex-state/src/components/App.jsx
+++ public/react/changing-complex-state/src/components/App.jsx
@@ -1,37 +1,51 @@
 import React, {useState} from "react";
 
 function App() {
-	const [firstName, setFirstName] = useState('');
-	const [lastName, setLastName] = useState('');
+	const [fullName, setFullName] = useState({
+		firstName: '',
+		lastName: '',
+	});
 
-	function updateFirstName(e) {
-		setFirstName(e.target.value);
-	}
+	function handleChange(event) {
+		const {name, value} = event.target;
 
-	function updateLastName(e) {
-		setLastName(e.target.value);
+		// useState 에서 예전 상태값 가져오기, 값 대신 함수를 넣어주면 가능.
+		// type SetStateAction<S> = S | ((prevState: S) => S);
+		setFullName(prevState => {
+			if (name === "fName") {
+				return {
+					...prevState,
+					firstName: value
+				}
+			} else {
+				return {
+					...prevState,
+					lastName: value
+				}
+			}
+		});
 	}
 
 	return (
-    <div className="container">
-      <h1>Hello {firstName} {lastName}</h1>
-      <form>
-        <input
-					onChange={updateFirstName}
-					value={firstName}
+		<div className="container">
+			<h1>Hello {fullName.firstName} {fullName.lastName}</h1>
+			<form>
+				<input
+					onChange={handleChange}
+					value={fullName.firstName}
 					name="fName"
 					placeholder="First Name"
 				/>
-        <input
-					onChange={updateLastName}
-					value={lastName}
+				<input
+					onChange={handleChange}
+					value={fullName.lastName}
 					name="lName"
 					placeholder="Last Name"
 				/>
-        <button>Submit</button>
-      </form>
-    </div>
-  );
+				<button>Submit</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
