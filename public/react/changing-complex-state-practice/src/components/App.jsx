997c6bf36f0d62ee53af5569c509dcba3fd1c3c6
wheejinv <wheejinv@naver.com>
2022-02-01 15:55:13 +0900

    섹션 33: React.js - 428 ~ 429. Javascript ES6 Spread Operator #1 complete

--- public/react/changing-complex-state-practice/src/components/App.jsx
+++ public/react/changing-complex-state-practice/src/components/App.jsx
@@ -7,6 +7,19 @@ function App() {
     email: ""
   });
 
+	function handleChange(e) {
+		const {name, value} = e.target;
+
+		setContact(prevState => {
+			let result = {
+				...prevState,
+				[name]: value // result[name] = value;
+			};
+
+			return result;
+		});
+	}
+
   return (
     <div className="container">
       <h1>
@@ -14,9 +27,24 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input
+					name="fName"
+					placeholder="First Name"
+					value={contact.fName}
+					onChange={handleChange}
+				/>
+        <input
+					name="lName"
+					placeholder="Last Name"
+					value={contact.lName}
+					onChange={handleChange}
+				/>
+        <input
+					name="email"
+					placeholder="Email"
+					value={contact.email}
+					onChange={handleChange}
+				/>
         <button>Submit</button>
       </form>
     </div>
