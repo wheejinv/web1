9220f000dec64f360ed6ad25d2f91fcf45ae1475
wheejinv <wheejinv@naver.com>
2022-02-02 23:27:00 +0900

    섹션 33: React.js - 434. React Dependencies & Styling the Keeper App #2 complete MaterialUI 사용: react 버전의 부트스트랩. https://mui.com/components/material-icons/ css 에 배경 이미지 설정: https://www.transparenttextures.com/

--- public/react/keeper-app-part3-styling/src/components/CreateArea.jsx
+++ public/react/keeper-app-part3-styling/src/components/CreateArea.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import {Fab, Zoom} from "@mui/material";
 
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
+	const [isExpanded, setIsExpanded] = useState(false);
 
-  function handleChange(event) {
+	function handleChange(event) {
     const { name, value } = event.target;
 
     setNote(prevNote => {
@@ -23,26 +26,34 @@ function CreateArea(props) {
       title: "",
       content: ""
     });
+		setIsExpanded(false);
     event.preventDefault();
   }
 
   return (
     <div>
       <form className="create-note">
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
-        <textarea
-          name="content"
-          onChange={handleChange}
-          value={note.content}
-          placeholder="Take a note..."
-          rows="3"
-        />
-        <button onClick={submitNote}>Add</button>
+				{isExpanded && <input
+					name="title"
+					onChange={handleChange}
+					value={note.title}
+					placeholder="Title"
+				/>}
+				<textarea
+					name="content"
+					onChange={handleChange}
+					onClick={() => {
+						setIsExpanded(true);
+					}}
+					value={note.content}
+					placeholder="Take a note..."
+					rows={isExpanded ? 3 : 1}
+				/>
+				{isExpanded && <Zoom in={true}>
+					<Fab onClick={submitNote}>
+						<AddIcon/>
+					</Fab>
+				</Zoom>}
       </form>
     </div>
   );
--- public/react/keeper-app-part3-styling/src/components/Header.jsx
+++ public/react/keeper-app-part3-styling/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import {Highlight} from "@mui/icons-material";
 
 function Header() {
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1><Highlight /> Keeper</h1>
     </header>
   );
 }
