09002481681cfd63a557e0a18633154480091764
wheejinv <wheejinv@naver.com>
2022-02-02 19:47:49 +0900

    섹션 33: React.js - 432. Managing a Component Tree Practice #2 complete

--- public/react/managing-a-component-tree-practice/src/components/InputArea.jsx
+++ public/react/managing-a-component-tree-practice/src/components/InputArea.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
-function InputArea() {
-  return (
+function InputArea(props) {
+	const {addItem} = props;
+
+	const [inputText, setInputText] = useState('');
+
+	function handleChange(e) {
+		let {value} = e.target;
+
+		setInputText(value);
+	}
+
+	function handleClick() {
+		addItem(inputText);
+
+		setInputText('');
+	}
+
+	return (
     <div className="form">
       <input onChange={handleChange} type="text" value={inputText} />
-      <button onClick={addItem}>
+      <button onClick={handleClick}>
         <span>Add</span>
       </button>
     </div>
