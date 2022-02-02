f784354c15fff28a588e01f242f6cc4fedc1f630
wheejinv <wheejinv@naver.com>
2022-02-02 20:57:04 +0900

    섹션 33: React.js - 433. Keeper App Project - Part 3 #2 complete https://codesandbox.io/s/keeper-part-3-starting-pogqj?fontsize=14&hidenavigation=1&theme=dark

--- public/react/keeper-app-part3/src/components/App.jsx
+++ public/react/keeper-app-part3/src/components/App.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
-  return (
+	let idCount = 1;
+
+	const [noteState, setNoteState] = useState([{
+		id: idCount,
+		title: "Note title",
+		content: "Note content",
+	}]);
+
+	function addNote(note) {
+		idCount++;
+
+		let {title, content} = note;
+
+		setNoteState( prevState => {
+			return [...prevState, {
+				id: idCount,
+				title,
+				content
+			}]
+		})
+	}
+
+	function deleteNote(id) {
+		setNoteState(prevState => prevState.filter( note => {
+			return note.id !== id
+		}))
+	}
+
+	return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea addNote={addNote} />
+			{noteState.map( note => {
+				return <Note deleteNote={deleteNote} key={note.id} id={note.id} title={note.title} content={note.content} />;
+			})}
+
       <Footer />
     </div>
   );
--- public/react/keeper-app-part3/src/components/CreateArea.jsx
+++ public/react/keeper-app-part3/src/components/CreateArea.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
-  return (
+function CreateArea(props) {
+	const [note, setNote] = useState({
+		title: '',
+		content: '',
+	});
+
+	function handleChange(e) {
+		let {name, value} = e.target;
+
+		if (name !== 'title' && name !== 'content') {
+			return;
+		}
+
+		setNote( prevState => {
+			return {
+				...prevState,
+				[name]: value
+			}
+		})
+	}
+
+	return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input onChange={handleChange} name="title" placeholder="Title" value={note.title} />
+        <textarea onChange={handleChange} name="content" placeholder="Take a note..." value={note.content} rows="3" />
+        <button onClick={ (e) => {
+					e.preventDefault();
+					props.addNote(note);
+
+					setNote({
+						title: '',
+						content: ''
+					})
+				}}>Add</button>
       </form>
     </div>
   );
