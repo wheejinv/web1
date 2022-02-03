da7fa95d53f4bb211ec2831cfae334a0a77e8aab
wheejinv <wheejinv@naver.com>
2022-02-03 22:37:59 +0900

    리액트를 다루는 기술 - 5장. ref: DOM에 이름 달기 #3 ValidationSample - ref를 사용하여 검증버튼을 누르고 나서도 input 창에 포커스 활성화. https://ko.reactjs.org/docs/refs-and-the-dom.html

--- learning-react/05-hello-react-ref/src/components/ValidationSample.js
+++ learning-react/05-hello-react-ref/src/components/ValidationSample.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './ValidationSample.scss'
 
 class ValidationSample extends Component {
+	// textInput DOM 엘리먼트를 저장하기 위한 ref를 생성합니다.
+	textInput = React.createRef();
 	state = {
 		password: '',
 		clicked: false,
@@ -19,13 +21,19 @@ class ValidationSample extends Component {
 			clicked: true,
 			validated: this.state.password === '0000'
 		});
-		this.input.current.focus();
+
+		// DOM API를 사용하여 명시적으로 text 타입의 input 엘리먼트를 포커스합니다.
+		// 주의: 우리는 지금 DOM 노드를 얻기 위해 "current" 프로퍼티에 접근하고 있습니다.
+		this.textInput.current.focus();
 	}
 
 	render() {
+		// React에게 우리가 text 타입의 input 엘리먼트를
+		// 우리가 생성자에서 생성한 `textInput` ref와 연결하고 싶다고 이야기합니다.
 		return (
 			<div>
 				<input
+					ref={this.textInput}
 					type="password"
 					value={this.state.password}
 					onChange={this.handleChange}
