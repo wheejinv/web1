afc3cec25e16de693a79a3554f5c087768be4c84
wheejinv <wheejinv@naver.com>
2022-02-05 19:28:37 +0900

    React 완벽 가이드 - 섹션4. 리액트 State 및 이벤트 다루기 #2 user input handle - setState 시 함수형을 사용하자. 주석 참고

--- react-complete-guide/03-expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/03-expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ExpenseForm.scss'
 
 function ExpenseForm(props) {
-	function changeHandle(e) {
+	const [input, setInput] = useState({
+		title: "",
+		amount: "",
+		date: "",
+	});
+
+	function changeHandler(e) {
 		const {name, value} = e.target;
-		console.log(e)
+		// console.log(typeof value, value)
+
+		// 동시에 많은 상태 업데이트를 계획한다면 오래되거나 잘못된 상태 snapshot에 의존할 수도 있게 됨.
+		// 그래서 이 방법은 추천하지 않음.
+		// setInput({
+		// 	...input,
+		// 	[name]: value
+		// })
+
+		// 항상 최신 상태 snapshot에 기반해서 실행한다는 전제를 보장해줌.
+		setInput( prevState => {
+			return {
+				...prevState,
+				[name]: value
+			}
+		})
+	}
+
+	function submitHandler(e) {
+		const {title, amount, date} = input;
+		const expenseData = {
+			title, amount, date: new Date(date)
+		}
+
+		console.log(expenseData);
+
+		setInput({
+			title: "",
+			amount: "",
+			date: "",
+		});
+
+		e.preventDefault();
 	}
 
 	return (
@@ -13,29 +51,37 @@ function ExpenseForm(props) {
 				<div className="new-expense__control">
 					<label>Title</label>
 					<input
+						name="title"
 						type="text"
-						onChange={changeHandle}
+						onChange={changeHandler}
+						value={input.title}
 					/>
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
 					<input
+						name="amount"
 						type="number"
 						min="0.01"
 						step="0.01"
+						onChange={changeHandler}
+						value={input.amount}
 					/>
 				</div>
 				<div className="new-expense__control">
 					<label>Date</label>
 					<input
+						name="date"
 						type="date"
 						min="2019-01-01"
 						max="2022-12-31"
+						onChange={changeHandler}
+						value={input.date}
 					/>
 				</div>
 			</div>
 			<div className="new-expense__actions">
-				<button type="submit">Add Expense</button>
+				<button type="submit" onClick={submitHandler}>Add Expense</button>
 			</div>
 		</form>
 	);
