47dda3994a429ff104b064e490f84b274c41468c
wheejinv <wheejinv@naver.com>
2022-02-06 01:27:35 +0900

    React 완벽 가이드 - 섹션4. 리액트 State 및 이벤트 다루기 #4 dropdown - year selector 컴포넌트 추가

--- react-complete-guide/03-expense-tracker/src/components/Expenses/Expenses.js
+++ react-complete-guide/03-expense-tracker/src/components/Expenses/Expenses.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import './Expenses.scss'
 
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
 	const {items} = props;
+	const [filteredYear, setFilteredYear] = useState('2020');
+
+	function filterChangeHandler(selectedYear) {
+		console.log(selectedYear)
+		// setSelectedYear(year);
+	}
 
 	return (
 		// Card 랩퍼 컴포넌트를 사용해서 .card 클래스의 스타일을 적용시켰다. (모서리 둥글게)
 		<Card className='expenses'>
+			<ExpensesFilter
+				onChangeFilter={filterChangeHandler}
+				selected={filteredYear}
+			/>
 			{items.map(item => {
 				return (
 					<ExpenseItem
