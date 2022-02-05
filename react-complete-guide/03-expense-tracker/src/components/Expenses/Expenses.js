bb4f449fefca7d6c72e1153d8270c21863f9b9f6
wheejinv <wheejinv@naver.com>
2022-02-06 02:52:07 +0900

    React 완벽 가이드 - 섹션5. 렌더링 리스트 및 Conditional Content #2 조건부 렌더링 - 강사는 JSX 부분을 최대한 간결하게 을 쓰는걸 선호한다고 함.

--- react-complete-guide/03-expense-tracker/src/components/Expenses/Expenses.js
+++ react-complete-guide/03-expense-tracker/src/components/Expenses/Expenses.js
@@ -16,6 +16,22 @@ function Expenses(props) {
 	const filteredExpenses = expenses.filter(expense => {
 		return expense.date.getFullYear().toString() === filteredYear;
 	});
+
+	let expensesContent = <p>No expenses found.</p>;
+
+	if (filteredExpenses.length > 0) {
+		expensesContent = filteredExpenses.map(expense => {
+			return (
+				<ExpenseItem
+					key={expense.id}
+					title={expense.title}
+					amount={expense.amount}
+					date={expense.date}
+				/>
+			)
+		})
+	}
+
 	return (
 		// Card 랩퍼 컴포넌트를 사용해서 .card 클래스의 스타일을 적용시켰다. (모서리 둥글게)
 		<Card className='expenses'>
@@ -23,16 +39,7 @@ function Expenses(props) {
 				onChangeFilter={filterChangeHandler}
 				selected={filteredYear}
 			/>
-			{filteredExpenses.map(expense => {
-				return (
-					<ExpenseItem
-						key={expense.id}
-						title={expense.title}
-						amount={expense.amount}
-						date={expense.date}
-					/>
-				)
-			})}
+			{expensesContent}
 		</Card>
 	);
 }
