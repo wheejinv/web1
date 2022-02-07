ed62bf69c6a31de7650128c33176615cf9b72ec5
wheejinv <wheejinv@naver.com>
2022-02-08 00:54:22 +0900

    React 완벽 가이드 - 섹션 10: 리듀서(Reducer)를 사용하여 사이드 이펙트 처리 & 컨텍스트 API 사용하기 #4 useEffect - cleanup

--- react-complete-guide/10-side-effects-reducers-context-api/src/components/Login/Login.js
+++ react-complete-guide/10-side-effects-reducers-context-api/src/components/Login/Login.js
@@ -14,9 +14,19 @@ const Login = (props) => {
 	// useEffect의 주요 업무는 사이드 이펙트를 처리하는 것이다.
 	// HTTP 요청일수도 있고, 이메일이나 비밀번호 등으로 폼 유효성을 확인하고 업데이트하는 것도 사이드 이펙트이다.
 	useEffect(() => {
-		setFormIsValid(
-			enteredEmail.includes('@') && enteredPassword.trim().length > 6
-		);
+		let timeoutID = setTimeout( () => {
+			// 키 입력 후 500 ms 이후 실행. 이 함수를 api 함수라고 하면 네트워크 호출을 많이 줄일 수 있다.
+			// 디바운싱 기술이라고 함.
+			setFormIsValid(
+				enteredEmail.includes('@') && enteredPassword.trim().length > 6
+			);
+		}, 500)
+		// console.log('setTimeout')
+
+		return () => {
+			// console.log('clearTimeout')
+			clearTimeout(timeoutID);
+		}
 	}, [enteredEmail, enteredPassword]);
 
 
