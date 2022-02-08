b14be9856246b77251aba6dc654465c7829a89c6
wheejinv <wheejinv@naver.com>
2022-02-08 12:52:01 +0900

    React 완벽 가이드 - 섹션 10: 리듀서(Reducer)를 사용하여 사이드 이펙트 처리 & 컨텍스트 API 사용하기 #6 useReducer를 사용하도록 리팩토링 - enteredEmail, emailIsValid 두 상태를 useReducer를 사용하여 하나의 상태{value: String, isValid: Boolean} 를 갖도록 변경.

--- react-complete-guide/10-side-effects-reducers-context-api/src/components/Login/Login.js
+++ react-complete-guide/10-side-effects-reducers-context-api/src/components/Login/Login.js
@@ -1,18 +1,57 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+// 컴토넌트 함수 밖에서 정의된 이유가 있음.
+// 리듀서 함수 안에서는 컴포넌트 함수 안에서 만들어진 데이터는 필요하지 않기 때문
+const emailReducer = (state, action) => {
+	if (action.type === 'USER_INPUT') {
+		return {
+			value: action.val,
+			isValid: action.val.includes('@')
+		}
+	} else if (action.type === 'INPUT_BLUR') {
+		// 새로운 상태 스냅숏을 반환해야 하기 때문에 최싱 상태 스냅숏(state) 을 사용한다.
+		return {
+			value: state.value,
+			isValid: state.value.includes('@')
+		}
+	}
+
+	return {
+		value: '',
+		isValid: false
+	}
+}
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+	// state: 최산 상태 스냅샷
+	// dispatchFn: 상태 스냅샷을 업데이트 해주는 함수, 하나의 액션을 디스패치하는데 reducerFn 이 소비하게 됨.
+	// reducerFn: (prevState, action) => newState : 새로 업데이트된 상태를 반환
+	// initialState: 초기 상태값
+	// init: 초기 상태 설정.
+	// const [state, dispatch] = useReducer(reducer, initialState, init);
+
+	const [emailState, dispatchEmail] = useReducer(emailReducer, {
+		value: '',
+		isValid: null, // false 값이면 div className 에 invalid 값이 붙게 되어 오류처럼 보임.
+	});
+
+
+	const emailChangeHandler = (event) => {
+    // setEnteredEmail(event.target.value);
+		dispatchEmail({
+			type: 'USER_INPUT',
+			val: event.target.value
+		});
 
 		setFormIsValid(
 			// 리액트가 업데이트 하는 방법에 따라서 enteredPassword 가 최신 스냅샷이 아닐수도 있음
@@ -24,12 +63,14 @@ const Login = (props) => {
     setEnteredPassword(event.target.value);
 
 		setFormIsValid(
-			event.target.value.trim().length > 6 && enteredEmail.includes('@')
+			event.target.value.trim().length > 6 && emailState.isValid
 		);
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+		dispatchEmail({
+			type: 'INPUT_BLUR', // 인풋이 포커스를 읽어서 흐려졌다(blur)
+		})
   };
 
   const validatePasswordHandler = () => {
@@ -41,7 +82,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -49,14 +90,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
