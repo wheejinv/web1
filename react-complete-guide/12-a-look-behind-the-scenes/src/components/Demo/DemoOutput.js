a3c74d6e1a27286f00b49edf567f609b1ec6f60e
wheejinv <wheejinv@naver.com>
2022-02-11 01:46:10 +0900

    React 완벽 가이드 - 섹션 12: A Look Behind The Scenes Of React & Optimization Techniques #2 React.memo() 로 불필요한 재평가 방지.

--- react-complete-guide/12-a-look-behind-the-scenes/src/components/Demo/DemoOutput.js
+++ react-complete-guide/12-a-look-behind-the-scenes/src/components/Demo/DemoOutput.js
@@ -7,4 +7,13 @@ const DemoOutput = (props) => {
   return <MyParagraph>{props.show ? 'This is new!' : ''}</MyParagraph>;
 };
 
-export default DemoOutput;
+// props 값을 이전에 받은 값이랑 비교하는 역할을 함.
+// 부모 컴포넌트가 바뀌었지만 props 는 바뀌지 않는 경우 컴포넌트가 실행되지 않음.
+export default React.memo(DemoOutput);
+
+// React.memo 사용 비용 == 이전 props 값을 보관하고 비요하는 일.
+// React.memo 사용 안하는 경우 == 컴포넌트 재평가
+
+// 잘 사용하는 경우
+// props 의 내용이 잘 안변해야 함.
+// 컴포넌트 트리의 뿌리를 골라서 작업하는게 효율적. 자식 컴포넌트의 가지가 모두 잘라지게 끔.
