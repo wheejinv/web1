292e294851c0219f08dde4d26b3bf4ca7ba68504
wheejinv <wheejinv@naver.com>
2022-02-11 00:44:50 +0900

    React 완벽 가이드 - 섹션 11: Practice: Building a Food Order App #11 useEffect 훅 사용해서 장바구니 아이템 담을 때 마다 애니메이션 재생

--- react-complete-guide/11-practice-food-order-app/src/components/Layout/HeaderCartButton.js
+++ react-complete-guide/11-practice-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,39 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import CartIcon from "../Cart/CartIcon";
 import styles from './HeaderCartButton.module.css'
 import CartContext from "../../store/cart-context";
 
 function HeaderCartButton(props) {
 	const cartCtx = useContext(CartContext);
-
+	const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+	const items = cartCtx.items;
 	// 카트에 담겨있는 개수의 총합: 예) 스시 3개 담으면, 3이 표시되어야 함.
-	const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+	const numberOfCartItems = items.reduce((curNumber, item) => {
 		return curNumber + item.amount;
 	}, 0)
 
+	const btnClasses = `${styles.button}${btnIsHighlighted ? ' ' +styles.bump : ''}`;
+
+	// 카트의 숫자가 변했을 때 애니메이션을 넣고 싶다.
+	useEffect(() => {
+		if (cartCtx.items.length === 0) {
+			return;
+		}
+
+		setBtnIsHighlighted(true);
+
+		const timer = setTimeout( () => {
+			setBtnIsHighlighted(false);
+		}, 300)
+
+		return () => {
+			clearTimeout(timer);
+		}
+
+	}, [numberOfCartItems]);
+
 	return (
-		<button className={styles.button} onClick={props.onClick}>
+		<button className={btnClasses} onClick={props.onClick}>
 			<span className={styles.icon}>
 				<CartIcon/>
 			</span>
