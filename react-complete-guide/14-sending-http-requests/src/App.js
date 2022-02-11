8d6914ccb7fe20d83b441a9e2119997c6e762f3f
wheejinv <wheejinv@naver.com>
2022-02-12 05:24:58 +0900

    React 완벽 가이드 - 섹션 14: HTTP 요청 보내기 #5 network error handling

--- react-complete-guide/14-sending-http-requests/src/App.js
+++ react-complete-guide/14-sending-http-requests/src/App.js
@@ -6,23 +6,38 @@ import './App.css';
 function App() {
 	const [movies, setMovies] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
 
 	const fetchMovieHandler = async () => {
 		setIsLoading(true);
+		setError(null);
 
-		const response = await fetch('https://swapi.dev/api/films/');
-		const json = await response.json();
+		try {
+			const response = await fetch('https://swapi.dev/api/films/');
 
-		let fetchedMovieData = json.results.map( (item, index) => {
-			return {
-				id: item.episode_id,
-				title: item.title,
-				openingText: item.opening_crawl,
-				releaseDate: item.release_date,
+			// 잘못된 url 요청이라면 이 부분에서 에러가 발생. Unexpected token < in JSON at position 1
+			const json = await response.json();
+
+			// 요청까지 잘 보냈는데 에러 응답 코드를 받게 되는 경우, 404, 500, 401
+			// response.status
+			if (!response.ok) {
+				throw new Error('Something went wrong!');
 			}
-		});
 
-		setMovies(fetchedMovieData);
+			let fetchedMovieData = json.results.map( (item, index) => {
+				return {
+					id: item.episode_id,
+					title: item.title,
+					openingText: item.opening_crawl,
+					releaseDate: item.release_date,
+				}
+			});
+
+			setMovies(fetchedMovieData);
+		} catch(e) {
+			setError(e.message);
+		}
+
 		setIsLoading(false);
 	}
 
@@ -35,8 +50,9 @@ function App() {
 				{!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
 
 				{/*폴백 예시*/}
-				{!isLoading && movies.length == 0 && <p>Found No Movies.</p>}
+				{!isLoading && movies.length == 0 && !error && <p>Found No Movies.</p>}
 				{isLoading && <p>데이터 가져오는 중...</p>}
+				{!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
