2fe17315f442374a3ea4f0ed42dc99390ed553c2
wheejinv <wheejinv@naver.com>
2022-02-12 00:48:14 +0900

    React 완벽 가이드 - 섹션 13: 컴포넌트를 구축하는 다른 방법: 클래스 컴포넌트 #4 클래스형 컴포넌트로 useEffect 의 dependencies 구현

--- react-complete-guide/13-class-based-cmp/src/components/UserFinder.js
+++ react-complete-guide/13-class-based-cmp/src/components/UserFinder.js
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect } from 'react';
+import {Fragment, useState, useEffect, Component} from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
@@ -9,28 +9,42 @@ const DUMMY_USERS = [
 	{ id: 'u3', name: 'Julie' },
 ];
 
-const UserFinder = () => {
-	const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
-	const [searchTerm, setSearchTerm] = useState('');
+class UserFinder extends Component {
+	constructor(props) {
+		super(props);
 
-	useEffect(() => {
-		setFilteredUsers(
-			DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+		this.state = {
+			filteredUsers: DUMMY_USERS,
+			searchTerm: '',
+		}
+	}
+
+	searchChangeHandler(event) {
+		this.setState({
+			searchTerm: event.target.value
+		})
+	}
+
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		if (prevState.searchTerm !== this.state.searchTerm) {
+			this.setState({
+				filteredUsers: DUMMY_USERS.filter((user) => {
+					return user.name.includes(this.state.searchTerm)
+				})
+			});
+		}
+	}
+
+	render() {
+		return (
+			<Fragment>
+				<div className={classes.finder}>
+					<input type='search' onChange={this.searchChangeHandler.bind(this)}/>
+				</div>
+				<Users users={this.state.filteredUsers}/>
+			</Fragment>
 		);
-	}, [searchTerm]);
-
-	const searchChangeHandler = (event) => {
-		setSearchTerm(event.target.value);
-	};
-
-	return (
-		<Fragment>
-			<div className={classes.finder}>
-				<input type='search' onChange={searchChangeHandler} />
-			</div>
-			<Users users={filteredUsers} />
-		</Fragment>
-	);
-};
+	}
+}
 
 export default UserFinder;
