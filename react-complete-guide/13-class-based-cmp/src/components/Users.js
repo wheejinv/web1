88468eb828baf091ac8f2c2125f931a8a68e9e2d
wheejinv <wheejinv@naver.com>
2022-02-12 00:11:23 +0900

    React 완벽 가이드 - 섹션 13: 컴포넌트를 구축하는 다른 방법: 클래스 컴포넌트 #3 useEffect 를 사용하는 함수형 컴포넌트를 클래스형 컴포넌트로 변환하기 전 단계 셋팅

--- react-complete-guide/13-class-based-cmp/src/components/Users.js
+++ react-complete-guide/13-class-based-cmp/src/components/Users.js
@@ -1,17 +1,11 @@
-import {Component, useState} from 'react';
+import {Component} from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
-
 class Users extends Component {
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
 
 		this.state = {
 			showUsers: true,
@@ -34,7 +28,7 @@ class Users extends Component {
 	render() {
 		const usersList = (
 			<ul>
-				{DUMMY_USERS.map((user) => (
+				{this.props.users.map((user) => (
 					<User key={user.id} name={user.name} />
 				))}
 			</ul>
