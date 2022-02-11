54bfb6759ad2eaf3ffe6aecd46d799ca8eaef61a
wheejinv <wheejinv@naver.com>
2022-02-12 04:38:17 +0900

    React 완벽 가이드 - 섹션 14: HTTP 요청 보내기 #4 로딩 상태 처리 및 폴백 예시

--- react-complete-guide/14-sending-http-requests/src/App.js
+++ react-complete-guide/14-sending-http-requests/src/App.js
@@ -4,28 +4,14 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-	const dummyMovies = [
-		{
-			id: 1,
-			title: 'Some Dummy Movie',
-			openingText: 'This is the opening text of the movie',
-			releaseDate: '2021-05-18',
-		},
-		{
-			id: 2,
-			title: 'Some Dummy Movie 2',
-			openingText: 'This is the second opening text of the movie',
-			releaseDate: '2021-05-19',
-		},
-	];
-
-	const [movieState, setMovieState] = useState(dummyMovies);
+	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
 	const fetchMovieHandler = async () => {
+		setIsLoading(true);
+
 		const response = await fetch('https://swapi.dev/api/films/');
 		const json = await response.json();
-
-		console.log(json)
 
 		let fetchedMovieData = json.results.map( (item, index) => {
 			return {
@@ -36,7 +22,8 @@ function App() {
 			}
 		});
 
-		setMovieState(fetchedMovieData);
+		setMovies(fetchedMovieData);
+		setIsLoading(false);
 	}
 
   return (
@@ -45,7 +32,11 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movieState} />
+				{!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+
+				{/*폴백 예시*/}
+				{!isLoading && movies.length == 0 && <p>Found No Movies.</p>}
+				{isLoading && <p>데이터 가져오는 중...</p>}
       </section>
     </React.Fragment>
   );
