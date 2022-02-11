ed73ecd456d39a5d6c701f142b2674fee8a0112c
wheejinv <wheejinv@naver.com>
2022-02-12 04:29:16 +0900

    React 완벽 가이드 - 섹션 14: HTTP 요청 보내기 #3 스타워즈 API 이용해서 영화 데이터 fetch

--- react-complete-guide/14-sending-http-requests/src/App.js
+++ react-complete-guide/14-sending-http-requests/src/App.js
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+	const dummyMovies = [
+		{
+			id: 1,
+			title: 'Some Dummy Movie',
+			openingText: 'This is the opening text of the movie',
+			releaseDate: '2021-05-18',
+		},
+		{
+			id: 2,
+			title: 'Some Dummy Movie 2',
+			openingText: 'This is the second opening text of the movie',
+			releaseDate: '2021-05-19',
+		},
+	];
+
+	const [movieState, setMovieState] = useState(dummyMovies);
+
+	const fetchMovieHandler = async () => {
+		const response = await fetch('https://swapi.dev/api/films/');
+		const json = await response.json();
+
+		console.log(json)
+
+		let fetchedMovieData = json.results.map( (item, index) => {
+			return {
+				id: item.episode_id,
+				title: item.title,
+				openingText: item.opening_crawl,
+				releaseDate: item.release_date,
+			}
+		});
+
+		setMovieState(fetchedMovieData);
+	}
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movieState} />
       </section>
     </React.Fragment>
   );
