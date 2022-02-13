d146ccf0b96fb5ce7a2c6df694ae5536ccb6b453
wheejinv <wheejinv@naver.com>
2022-02-14 00:50:40 +0900

    React 완벽 가이드 - 섹션 16: Form 및 사용자 입력 작업 #5 email 도 포함하고 email 과 name 이 만족했을 때 submit 버튼을 활성화

--- react-complete-guide/16-working-with-forms/src/components/SimpleInput.js
+++ react-complete-guide/16-working-with-forms/src/components/SimpleInput.js
@@ -7,8 +7,22 @@ const SimpleInput = (props) => {
 	const [enteredName, setEnteredName] = useState('');
 	const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
+	const [enteredEmail, setEnteredEmail] = useState('');
+	const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+
+	const [formIsValid, setFormIsValid] = useState(false);
+
 	const enteredNameIsValid = enteredName.trim() !== '';
 	const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+	const enteredEmailIsValid = enteredEmail.includes('@');
+	const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+
+	// 모든 폼의 유효성을 검사.
+	useEffect( () => {
+		setFormIsValid(enteredNameIsValid && enteredEmailIsValid);
+	}, [enteredNameIsValid, enteredEmailIsValid])
+
 
 
 	const nameInputChangeHandler = (e) => {
@@ -19,19 +33,28 @@ const SimpleInput = (props) => {
 		e.preventDefault();
 
 		setEnteredNameTouched(true);
+		setEnteredEmailTouched(true);
 
-		if (enteredNameIsValid === false) {
+		if (formIsValid === false) {
 			return;
 		}
 
-		console.log(enteredName);
-
 		setEnteredName('');
+		setEnteredEmail('');
 		setEnteredNameTouched(false);
+		setEnteredEmailTouched(false);
 	}
 
 	const nameInputBlurHandler = (e) => {
 		setEnteredNameTouched(true);
+	}
+
+	const emailInputChangeHandler = (e) => {
+		setEnteredEmail(e.target.value);
+	}
+
+	const emailInputBlurHandler = (e) => {
+		setEnteredEmailTouched(true);
 	}
 
 	return (
@@ -50,8 +73,22 @@ const SimpleInput = (props) => {
 				/>
 				{nameInputIsInvalid && <p className="error-text">Name must not be empty.</p>}
       </div>
+			<div className={cn(
+				'form-control',
+				{invalid: emailInputIsInvalid}
+			)}>
+				<label htmlFor='email'>Your Email</label>
+				<input
+					type='email'
+					id='email'
+					onChange={emailInputChangeHandler}
+					onBlur={emailInputBlurHandler}
+					value={enteredEmail}
+				/>
+				{emailInputIsInvalid && <p className="error-text">Email must has '@'</p>}
+			</div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
