0a80ac080924c39a57e510ce4df38c0ce6dd7c9f
wheejinv <wheejinv@naver.com>
2022-02-13 19:47:28 +0900

    React 완벽 가이드 - 섹션 16: Form 및 사용자 입력 작업 #2 validation

--- react-complete-guide/16-working-with-forms/src/components/SimpleInput.js
+++ react-complete-guide/16-working-with-forms/src/components/SimpleInput.js
@@ -1,9 +1,45 @@
+import {useRef, useState} from "react";
+import classNames from "classnames/bind";
+
+const cn = classNames.bind(classNames);
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
+	const [enteredName, setEnteredName] = useState('');
+	const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+	const nameInputRef = useRef();
+
+	const nameInputChangeHandler = (e) => {
+		setEnteredName(e.target.value);
+	}
+
+	const formSubmissionHandler = e => {
+		e.preventDefault();
+
+		// 유효성 검사
+		if (enteredName.trim() === '') {
+			setEnteredNameIsValid(false);
+			return;
+		}
+
+		setEnteredNameIsValid(true);
+		// setEnteredName('');
+	}
+
+	return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={cn(
+				'form-control',
+				{invalid: !enteredNameIsValid}
+			)}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+					ref={nameInputRef}
+					type='text'
+					id='name'
+					onChange={nameInputChangeHandler}
+					value={enteredName}
+				/>
+				{!enteredNameIsValid && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
