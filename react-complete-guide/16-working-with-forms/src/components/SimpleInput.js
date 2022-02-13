1feb41ff6e43c591f5d9f5bfe796c6127b8c8e1d
wheejinv <wheejinv@naver.com>
2022-02-13 20:40:57 +0900

    React 완벽 가이드 - 섹션 16: Form 및 사용자 입력 작업 #3 wat touched state 처리 하기.
    처음 submit 이후부터 이름 유효성 검증하도록 작업됨

--- react-complete-guide/16-working-with-forms/src/components/SimpleInput.js
+++ react-complete-guide/16-working-with-forms/src/components/SimpleInput.js
@@ -1,12 +1,22 @@
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import classNames from "classnames/bind";
 
 const cn = classNames.bind(classNames);
 
 const SimpleInput = (props) => {
 	const [enteredName, setEnteredName] = useState('');
-	const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
+	// true 설정부터가 이건 속임수이다. ㅋㅋ 이 상태가 달리 쓸모없음을 알고 있는거랑 마찬가지
+	const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+	const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 	const nameInputRef = useRef();
+
+	useEffect(() => {
+	if (enteredNameIsValid) {
+		console.log('Name Input is valid');
+	}
+	}, [enteredNameIsValid]);
+
 
 	const nameInputChangeHandler = (e) => {
 		setEnteredName(e.target.value);
@@ -14,6 +24,8 @@ const SimpleInput = (props) => {
 
 	const formSubmissionHandler = e => {
 		e.preventDefault();
+
+		setEnteredNameTouched(true);
 
 		// 유효성 검사
 		if (enteredName.trim() === '') {
@@ -24,12 +36,13 @@ const SimpleInput = (props) => {
 		setEnteredNameIsValid(true);
 		// setEnteredName('');
 	}
+	const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
 	return (
     <form onSubmit={formSubmissionHandler}>
       <div className={cn(
 				'form-control',
-				{invalid: !enteredNameIsValid}
+				{invalid: nameInputIsInvalid}
 			)}>
         <label htmlFor='name'>Your Name</label>
         <input
@@ -39,7 +52,7 @@ const SimpleInput = (props) => {
 					onChange={nameInputChangeHandler}
 					value={enteredName}
 				/>
-				{!enteredNameIsValid && <p className="error-text">Name must not be empty.</p>}
+				{nameInputIsInvalid && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
