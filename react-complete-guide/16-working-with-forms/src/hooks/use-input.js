707576c4b404ffbfe2d1e41175ccca6d4f75df53
wheejinv <wheejinv@naver.com>
2022-02-14 03:36:53 +0900

    React 완벽 가이드 - 섹션 16: Form 및 사용자 입력 작업 #8 리팩토링 custom hook 인 useInput 에서 useState 가 아닌 useReducer 사용하도록

--- react-complete-guide/16-working-with-forms/src/hooks/use-input.js
+++ react-complete-guide/16-working-with-forms/src/hooks/use-input.js
@@ -1,23 +1,52 @@
-import {useState} from "react";
+import {useReducer, useState} from "react";
+
+const initialInputState = {
+	enteredValue: '',
+	isTouched: false,
+}
+
+const inputStateReducer = (state, action) => {
+	if (action.type === 'INPUT') {
+		return {
+			...state,
+			enteredValue: action.val,
+		}
+	} else if (action.type === 'BLUR') {
+		return {
+			...state,
+			isTouched: true,
+		}
+	} else if (action.type === 'RESET') {
+		return initialInputState;
+	}
+}
 
 const useInput = (validateValue) => {
-	const [enteredValue, setEnteredValue] = useState('');
-	const [isTouched, setIsTouched] = useState(false);
+	const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState);
+
+	const {isTouched, enteredValue} = inputState;
 
 	const valueIsValid = validateValue(enteredValue);
 	const hasError = !valueIsValid && isTouched;
 
 	const valueChangeHandler = (e) => {
-		setEnteredValue(e.target.value);
+		dispatch({
+			type: 'INPUT',
+			val: e.target.value
+		});
 	}
 
 	const inputBlurHandler = (e) => {
-		setIsTouched(true);
+		dispatch({
+			type: 'BLUR',
+			val: true
+		});
 	}
 
 	const reset = () => {
-		setEnteredValue('');
-		setIsTouched(false);
+		dispatch({
+			type: 'RESET'
+		})
 	}
 
 	return {
