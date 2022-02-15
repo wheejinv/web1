8841ce79e89715a2c0732740c4911d8b4ec01334
wheejinv <wheejinv@naver.com>
2022-02-16 00:16:44 +0900

    React 완벽 가이드 - 섹션 17: 연습 프로젝트: 음식 주문 앱에 Http 및 양식 추가 #5 validate form input text

--- react-complete-guide/17-practice-food-order-http-forms/src/components/Cart/Checkout.js
+++ react-complete-guide/17-practice-food-order-http-forms/src/components/Cart/Checkout.js
@@ -1,7 +1,17 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import styles from './Checkout.module.css'
 
+const isEmpty = (value) => value.trim() === '';
+const isFiveChars = (value) => value.trim().length === 5;
+
 function Checkout(props) {
+	const [formInputsValidity, setFormInputsValidity] = useState({
+		name: true,
+		street: true,
+		city: true,
+		postalCode: true,
+	});
+
 	const nameInputRef = useRef();
 	const streetInputRef = useRef();
 	const cityInputRef = useRef();
@@ -14,29 +24,74 @@ function Checkout(props) {
 		const enteredName = nameInputRef.current.value;
 		const enteredStreet = streetInputRef.current.value;
 		const enteredCity = cityInputRef.current.value;
-		const enteredPostal = postalInputRef.current.value;
+		const enteredPostalCode = postalInputRef.current.value;
+
+		const enteredNameIsValid = !isEmpty(enteredName);
+		const enteredStreetIsValid = !isEmpty(enteredStreet);
+		const enteredCityIsValid = !isEmpty(enteredCity);
+		const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
+
+		setFormInputsValidity({
+			name: enteredNameIsValid,
+			street: enteredStreetIsValid,
+			city: enteredCityIsValid,
+			postalCode: enteredPostalCodeIsValid,
+		});
+
+		const formIsValid =
+			enteredNameIsValid &&
+			enteredStreetIsValid &&
+			enteredCityIsValid &&
+			enteredPostalCodeIsValid;
+
+		if (!formIsValid) {
+			return;
+		}
+
+		// Submit cart data
 	}
+
+	const nameControlClasses = `${styles.control} ${
+		formInputsValidity.name ? '' : styles.invalid
+	}`;
+	const streetControlClasses = `${styles.control} ${
+		formInputsValidity.street ? '' : styles.invalid
+	}`;
+	const postalCodeControlClasses = `${styles.control} ${
+		formInputsValidity.postalCode ? '' : styles.invalid
+	}`;
+	const cityControlClasses = `${styles.control} ${
+		formInputsValidity.city ? '' : styles.invalid
+	}`;
 
 	return (
 		<form onSubmit={submitHandler}>
-			<div className={styles.control}>
+			<div className={nameControlClasses}>
 				<label htmlFor="name">Your Name</label>
 				<input ref={nameInputRef} type="text" id="name"/>
+				{!formInputsValidity.name && <p>Please enter a valid name!</p>}
 			</div>
-			<div className={styles.control}>
+			<div className={streetControlClasses}>
 				<label htmlFor="street">Street</label>
 				<input ref={streetInputRef} type="text" id="street"/>
+				{!formInputsValidity.street && <p>Please enter a valid street!</p>}
 			</div>
-			<div className={styles.control}>
+			<div className={postalCodeControlClasses}>
 				<label htmlFor="postal">Postal</label>
 				<input ref={postalInputRef} type="text" id="postal"/>
+				{!formInputsValidity.postalCode && (
+					<p>Please enter a valid postal code (5 characters long)!</p>
+				)}
 			</div>
-			<div className={styles.control}>
+			<div className={cityControlClasses}>
 				<label htmlFor="city">City</label>
 				<input ref={cityInputRef} type="text" id="city"/>
+				{!formInputsValidity.city && <p>Please enter a valid city!</p>}
 			</div>
-			<button type="button" onClick={props.onCancel}>Cancel</button>
-			<button>Confirm</button> {/* 이 버튼만 submit 이 가능함. */}
+			<div className={styles.actions}>
+				<button type="button" onClick={props.onCancel}>Cancel</button>
+				<button className={styles.submit}>Confirm</button> {/* 이 버튼만 submit 이 가능함. */}
+			</div>
 		</form>
 	);
 }
