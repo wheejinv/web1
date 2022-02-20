44c6d88fc40ac44c0bf105d4fc89feb04df7a497
wheejinv <wheejinv@naver.com>
2022-02-20 12:06:29 +0900

    React 완벽 가이드 - 섹션 19: Advanced Redux #7 리팩토링: 액션 생성자 Thunk 사용하기. - 사이드 이펙트를 실행할 수 있는 액션 생성함수를 가지려 할 때 사용함. - 컴포넌트를 lean 하게 유지 가능,

--- react-complete-guide/19-advanced-redux/src/App.js
+++ react-complete-guide/19-advanced-redux/src/App.js
@@ -3,8 +3,8 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {useDispatch, useSelector} from "react-redux";
 import {Fragment, useEffect} from "react";
-import {uiActions} from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import {sendCartData} from "./store/cart-slice";
 
 let isInitial = true;
 
@@ -15,40 +15,12 @@ function App() {
 	const notification = useSelector((state) => state.ui.notification);
 
 	useEffect(() => {
-		const sendCartData = async () => {
-			dispatch(uiActions.showNotification({
-				status: 'pending',
-				title: 'Sending..',
-				message: 'Sending cart data!',
-			}));
-			const response = await fetch('https://whee-hello-firebase-default-rtdb.firebaseio.com/cart.json',{
-				method: 'PUT',
-				body: JSON.stringify(cart),
-			})
-
-			if (!response.ok) {
-				throw new Error('Sending cart data failed.');
-			}
-
-			dispatch(uiActions.showNotification({
-				status: 'success',
-				title: 'Success!',
-				message: 'Sent cart data successfully!',
-			}));
-		}
-
 		if (isInitial) {
 			isInitial = false;
 			return;
 		}
 
-		sendCartData().catch(e => {
-			dispatch(uiActions.showNotification({
-				status: 'error',
-				title: 'Error!',
-				message: 'Sending cart data failed!\n' + e.message,
-			}));
-		})
+		dispatch(sendCartData(cart))
 
 	// useDispatch에 의해 생성된 dispatch 의존성 추가 - react-redux가 이 함수는 절대 변하지 않을것이라고 보증함.
 	}, [cart, dispatch]);
--- react-complete-guide/19-advanced-redux/src/store/cart-slice.js
+++ react-complete-guide/19-advanced-redux/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {uiActions} from "./ui-slice";
 
 const initialCartState = {
 	items: [],
@@ -41,6 +42,50 @@ const cartSlice = createSlice({
 		},
 	}
 });
+
+// 사실상 함수인 액션을 disaptch 하고 있음. 액션 객체 대신.
+// 사이드 이펙트를 실행할 수 있는 액션 생성함수를 가지려 할 때 사용함.
+// 이는 Redux toolkit 을 사용할 때 Redux 안에 생겨날 것이다.
+// 컴포넌트를 lean 하게 유지하고 너무 많은 로직을 갖고 있지 않는 것도 그리 나쁜 생각이 아니다.
+export const sendCartData = (cart) => {
+	return async (dispatch) => {
+		dispatch(uiActions.showNotification({
+			status: 'pending',
+			title: 'Sending..',
+			message: 'Sending cart data!',
+		}));
+
+		const sendRequest = async () => {
+			const response = await fetch('https://whee-hello-firebase-default-rtdb.firebaseio.com/cart.json',{
+				method: 'PUT',
+				body: JSON.stringify(cart),
+			})
+
+			if (!response.ok) {
+				throw new Error('Sending cart data failed.');
+			}
+		}
+
+		try {
+			await sendRequest();
+
+			dispatch(
+				uiActions.showNotification({
+					status: 'success',
+					title: 'Success!',
+					message: 'Sent cart data successfully!',
+				})
+			);
+
+		} catch (e) {
+			dispatch(uiActions.showNotification({
+				status: 'error',
+				title: 'Error!',
+				message: 'Sending cart data failed!\n' + e.message,
+			}));
+		}
+	}
+}
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
