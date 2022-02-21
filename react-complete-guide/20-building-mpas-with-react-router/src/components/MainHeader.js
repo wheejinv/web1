10403125fe2b9fb8b17d266f33a239e195937f04
wheejinv <wheejinv@naver.com>
2022-02-21 22:56:33 +0900

    React 완벽 가이드 - 섹션 20: React-router 가 있는 SPA 다중 페이지 구축 #4 NavLink 사용하기.
    강의에서는 v5 기준으로 설명하고 있고, 난 v6 설치해서 문법이 많이 다름.
    아래 링크는 마이그레이션 방법을 나타냄.
    https://reactrouter.com/docs/en/v6/upgrading/v5#rename-navlink-exact-to-navlink-end

--- react-complete-guide/20-building-mpas-with-react-router/src/components/MainHeader.js
+++ react-complete-guide/20-building-mpas-with-react-router/src/components/MainHeader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './MainHeader.module.css'
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 
 function MainHeader(props) {
 	return (
@@ -10,10 +10,20 @@ function MainHeader(props) {
 					<li>
 						{/*Link는 클릭을 listen 해서 브라우저 기본값을 막는다.*/}
 						{/*대신 수동으로 URL을 업데이트해서 페이지가 전환된 것처럼 보이도록 함.*/}
-						<Link to="/welcome">Welcome</Link>
+						<NavLink
+							to="/welcome"
+							className={({ isActive }) => (isActive ? styles.active : "")}
+						>Welcome</NavLink>
 					</li>
 					<li>
-						<Link to="/products">Products</Link>
+						{/*NavLink는 CSS 스타일을 넣어 줄 수 있음.
+						리액트 웹의 현재 URL과 to가 가리키는 링크가 일치할 때, isActive 변수가 true 임. */}
+						<NavLink
+							to="/products"
+							// react-router-dom v5 문법과 v6 문법은 많이 달라졌음.
+							// https://reactrouter.com/docs/en/v6/upgrading/v5#rename-navlink-exact-to-navlink-end
+							className={({ isActive }) => (isActive ? styles.active : "")}
+						>Products</NavLink>
 					</li>
 				</ul>
 			</nav>
