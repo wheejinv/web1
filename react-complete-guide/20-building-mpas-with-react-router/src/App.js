a288d544ff6c6ef1f582974d72675793ba52c3c0
wheejinv <wheejinv@naver.com>
2022-02-22 22:28:02 +0900

    React 완벽 가이드 - 섹션 20: React-router 가 있는 SPA 다중 페이지 구축 #5 params 을 사용해서 동적 경로 추가하고 useParam hook으로 경로 매개 변수 추출

--- react-complete-guide/20-building-mpas-with-react-router/src/App.js
+++ react-complete-guide/20-building-mpas-with-react-router/src/App.js
@@ -2,6 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
 				<Routes>
 					<Route path="/welcome" element={<Welcome/>}/>
 					<Route path="/products" element={<Products/>}/>
+					{/*productId: 동적 경로 세그먼트 - 아무값이나 받을 수 있음.*/}
+					<Route path="/products/:productId" element={<ProductDetail/>}/>
 				</Routes>
 			</main>
 		</div>
