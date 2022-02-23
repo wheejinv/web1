0018869f9ce5b1765abb4884dbf8bcde83d8fb76
wheejinv <wheejinv@naver.com>
2022-02-23 20:07:54 +0900

    React 완벽 가이드 - 섹션 20-2: React-router 연습 #6 useNavigate
    react-router-dom v5는 useHistory 를 사용하고 v6는 useNavigate 를 사용함.

--- react-complete-guide/20-2-time-to-practice-route/src/pages/NewQuote.js
+++ react-complete-guide/20-2-time-to-practice-route/src/pages/NewQuote.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import QuoteForm from "../components/quotes/QuoteForm";
+import {useNavigate} from "react-router-dom";
 
 function NewQuote(props) {
+	const navigate = useNavigate();
+
 	const addQuoteHandler = quoteData => {
 		console.log(quoteData);
+
+		navigate('/quotes', {replace: true});
 	}
 
 	return (
