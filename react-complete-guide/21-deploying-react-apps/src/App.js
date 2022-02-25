e7ec8ce0c2339c05f8d52dc0214e59af637884c6
wheejinv <wheejinv@naver.com>
2022-02-25 16:48:58 +0900

    React 완벽 가이드 - 섹션 21: 리액트 앱 배포하기 #3 React.lazy 로딩
    서버에 배포 시에는 /some-route 라는 경로를 무시하도록 해야하고, 서버 호스팅 측에서 그렇게 하지 않는게 디폴트임.
    특정 호스트 제공자의 문서를 살펴야 하며, 강의에 특장하는 파이어베이스는 그 설정이 쉽다.

--- react-complete-guide/21-deploying-react-apps/src/App.js
+++ react-complete-guide/21-deploying-react-apps/src/App.js
@@ -1,23 +1,35 @@
-import {Navigate, Routes, Route} from "react-router-dom";
-import AllQuotes from "./pages/AllQuotes";
-import NotFound from "./pages/NotFound";
-import QuoteDetail from "./pages/QuoteDetail";
-import NewQuote from "./pages/NewQuote";
-import Comments from "./components/comments/Comments";
+import React, {Suspense} from "react";
+import {Navigate, Route, Routes} from "react-router-dom";
 import Layout from "./components/layout/Layout";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+// React.lazy 에서 전달하는 이 함수는 이 NewQuote 컴포넌트가 필요할 때 React에 의해 실행됨.
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+const Comments = React.lazy(() => import('./components/comments/Comments'));
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
+
+// { } 들어가면 동작안함 주의 - 아마 return 까지 붙여야하는듯
+// https://stackoverflow.com/questions/65183174/uncaught-undefined-error-when-using-react-lazy/66281145#66281145?newreg=dd27bff0a5c64c779053efe5e9393e58
+// const MyComponent = React.lazy(() => {
+// 	import('./scenes/MyComponent')
+// )};
 
 function App() {
 	return (
 		<Layout>
-			<Routes>
-				<Route path="/" element={<Navigate to="/quotes"/>}/>
-				<Route path="/quotes" element={<AllQuotes/>}/>
-				<Route path="/quotes/:quoteId" element={<QuoteDetail/>}>
-					<Route path="comments" element={<Comments/>}/>
-				</Route>
-				<Route path="/new-quote" element={<NewQuote/>}/>
-				<Route path="*" element={<NotFound/>}/>
-			</Routes>
+			<Suspense fallback={<div className="centered"><LoadingSpinner/></div>}>
+				<Routes>
+					<Route path="/" element={<Navigate to="/quotes"/>}/>
+					<Route path="/quotes" element={<AllQuotes/>}/>
+					<Route path="/quotes/:quoteId" element={<QuoteDetail/>}>
+						<Route path="comments" element={<Comments/>}/>
+					</Route>
+					<Route path="/new-quote" element={<NewQuote/>}/>
+					<Route path="*" element={<NotFound/>}/>
+				</Routes>
+			</Suspense>
 		</Layout>
 	);
 }
