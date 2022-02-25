f83a07ed965a4d24ea92bc5558f9d4818a43efff
wheejinv <wheejinv@naver.com>
2022-02-25 20:53:08 +0900

    React 완벽 가이드 - 섹션 22: 리액트 앱 인증 추가하기 #2 sign up & sign in, alert 으로 유저에게 피드백 주기

--- react-complete-guide/22-authentication/src/components/Auth/AuthForm.js
+++ react-complete-guide/22-authentication/src/components/Auth/AuthForm.js
@@ -1,28 +1,84 @@
-import { useState } from 'react';
+import {useRef, useState} from 'react';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+	const [isLoading, setIsLoading] = useState(false);
+	const emailInputRef = useRef();
+	const passwordInputRef = useRef();
 
-  const switchAuthModeHandler = () => {
+	const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
+
+	const submitHandler = async (e) => {
+		e.preventDefault();
+
+		const enteredEmail = emailInputRef.current.value;
+		const enteredPassword = passwordInputRef.current.value;
+
+		setIsLoading(true);
+
+		let url;
+
+		if (isLogin) {
+			// sign in with email/password
+			// https://firebase.google.com/docs/reference/rest/auth#section-sign-in-email-password
+			url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBB9cTkT4hgho6EG6jS1c5Z5rgn67LB6es';
+		} else {
+			// sign up with email/password
+			// https://firebase.google.com/docs/reference/rest/auth#section-create-email-password
+			url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBB9cTkT4hgho6EG6jS1c5Z5rgn67LB6es';
+		}
+		try {
+			const response = await fetch(url, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					email: enteredEmail,
+					password: enteredPassword,
+					returnSecureToken: true,
+				})
+			});
+
+			setIsLoading(false);
+
+			const json = await response.json();
+
+			if (response.ok) {
+				console.log('sign in, up ok', json);
+			} else {
+				console.log(json);
+				let errorMessage = 'Authentication failed!';
+				if (json && json.error && json.error.message) {
+					errorMessage = json.error.message;
+					throw new Error(errorMessage);
+				}
+			}
+		} catch (e) {
+			alert(e);
+		}
+
+	}
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input ref={emailInputRef} type='email' id='email' required />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input ref={passwordInputRef} type='password' id='password' required />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+					{!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+					{isLoading && <p>Sending request....</p>}
           <button
             type='button'
             className={classes.toggle}
