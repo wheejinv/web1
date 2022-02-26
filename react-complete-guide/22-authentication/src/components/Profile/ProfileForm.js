59e392912e10746537b7f13855b2d0d1ec370f36
wheejinv <wheejinv@naver.com>
2022-02-26 12:59:53 +0900

    React 완벽 가이드 - 섹션 22: 리액트 앱 인증 추가하기 #4 보호된 리소스에 대한 요청에 토큰 사용하기 - change password

--- react-complete-guide/22-authentication/src/components/Profile/ProfileForm.js
+++ react-complete-guide/22-authentication/src/components/Profile/ProfileForm.js
@@ -1,11 +1,44 @@
 import classes from './ProfileForm.module.css';
+import {useContext, useRef} from "react";
+import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
+	const newPasswordInputRef = useRef();
+	const authCtx = useContext(AuthContext);
+
+	const submitHandler = async e => {
+		e.preventDefault();
+
+		// change password
+		// https://firebase.google.com/docs/reference/rest/auth#section-change-password
+		const url = 'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBB9cTkT4hgho6EG6jS1c5Z5rgn67LB6es'
+
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				idToken: authCtx.token,
+				password: newPasswordInputRef.current.value,
+				returnSecureToken: false,
+			})
+		});
+
+		const json = response.json();
+
+		// todo 응답값에 따른 피드백 주기
+	}
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input
+					ref={newPasswordInputRef}
+					minLength={7}  // 최소 7글자
+					type='password'
+					id='new-password' />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
