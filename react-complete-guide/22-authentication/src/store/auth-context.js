b271cb4660301d1d98d313be23ffd6ada7b0994b
wheejinv <wheejinv@naver.com>
2022-02-26 22:43:58 +0900

    React 완벽 가이드 - 섹션 22: 리액트 앱 인증 추가하기 #8 자동로그아웃 - 로그인 시점에도 토큰이 유효한지 확인.
    logoutHandler useCallback 은 useEffect 사용으로 dependency 목록으로 들어가서 쓰게 됨.

--- react-complete-guide/22-authentication/src/store/auth-context.js
+++ react-complete-guide/22-authentication/src/store/auth-context.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 
 const AuthContext = React.createContext({
 	token: null,
@@ -14,26 +14,69 @@ const getRemainingTime = expirationTime => {
 	return expirationTime - current;
 }
 
+let logoutTimer;
+
+const retrieveStoredToken = () => {
+	const storedToken = localStorage.getItem('token');
+	const storedExpirationDate = localStorage.getItem('expirationTime');
+
+	const remainingTime = getRemainingTime(+storedExpirationDate);
+
+	if (remainingTime < 0) {
+		localStorage.removeItem('token');
+		localStorage.removeItem('expirationTime');
+		return null;
+	}
+
+	return {
+		token: storedToken,
+		remainingTime,
+	}
+}
+
 export const AuthContextProvider = (props) => {
-	const initialToken = localStorage.getItem('token');
+	const tokenData = retrieveStoredToken();
+
+	let initialToken;
+
+	if (tokenData) {
+		initialToken = tokenData.token;
+	}
+
 	const [token, setToken] = useState(initialToken);
 
 	const loginHandler = (token, expirationTime) => {
 		setToken(token);
 		localStorage.setItem('token', token);
-		localStorage.setItem('expirationTime', '' + expirationTime);
+		localStorage.setItem('expirationTime', expirationTime);
 
 		const remainingTime = getRemainingTime(expirationTime);
 
-		setTimeout(logoutHandler, remainingTime);
+		logoutTimer = setTimeout(logoutHandler, remainingTime);
 	}
 
-	const logoutHandler = () => {
-		setToken(null);
-		localStorage.removeItem('token');
-	}
+	// 디펜던시 관점에서의 주석
+	const logoutHandler = useCallback(() => {
+		setToken(null); // 리액트가 이 함수를 절대 바꾸지 않음.
+		localStorage.removeItem('token'); // 브라우저 빌드인 함수
+		localStorage.removeItem('expirationTime');
+
+		if (logoutTimer) {
+			// clearTimeout: 브라우저 빌트인 함수
+			// logoutTimer: 글로벌 변수, 리액트 렌더링 흐름 바깥에 있으므로 추가할 필요 없음.
+			clearTimeout(logoutTimer);
+			logoutTimer = null;
+		}
+	}, []);
 
 	let isLoggedIn = !!token;
+
+	useEffect(() => {
+		if (tokenData) {
+			logoutTimer = setTimeout(logoutHandler, tokenData.remainingTime);
+		}
+	}, [tokenData, logoutHandler]);
+
 
 	const contextValue = {
 		token,
