60c27c33682fd61a69da48bef9119fb6dbf01cbb
wheejinv <wheejinv@naver.com>
2022-02-27 18:22:50 +0900

    React 완벽 가이드 - 섹션 23: Next.js 소개 #4 extracting dynamic parameter value
    router.query

--- react-complete-guide/23-nextjs-introduction/pages/news/[newsId].js
+++ react-complete-guide/23-nextjs-introduction/pages/news/[newsId].js
@@ -1,7 +1,14 @@
 // 파일이름에 대괄호가 있으면 NextJS 가 이건 동적 페이지라는 걸 안다.
 
+import {useRouter} from "next/router";
+
 function DetailPage(props) {
-	console.log(props)
+	const router = useRouter();
+
+	const newsId = router.query.newsId;
+
+	// send a request to the backend API
+	// to fetch the news item with newsId
 
 	return (
 		<h1>The Detail Page</h1>
