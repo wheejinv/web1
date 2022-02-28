934601b82c8e061c38c80d6ea73d3399b7195770
wheejinv <wheejinv@naver.com>
2022-02-28 19:57:49 +0900

    React 완벽 가이드 - 섹션 23-2: Next.js 로 MeetUps 프로젝트 #4 router.push 함수를 이용해서 프로그래밍 방식으로 navigate

--- react-complete-guide/23-2-nextjs-meetups/components/meetups/MeetupItem.js
+++ react-complete-guide/23-2-nextjs-meetups/components/meetups/MeetupItem.js
@@ -1,7 +1,15 @@
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
+import {useRouter} from "next/router";
 
 function MeetupItem(props) {
+	const router = useRouter();
+
+	const showDetailHandler = e => {
+		// push 함수: 새 페이지를 페이지 더미로 보낸다. 동등하게 링크 컴포넌트를 이용함.
+		router.push(`/${props.id}`);
+	}
+
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +21,8 @@ function MeetupItem(props) {
           <address>{props.address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+					{/*Link 컴포넌트로 해도 되지만 프로그래밍으로 내비게이션 하는 방법*/}
+          <button onClick={showDetailHandler}>Show Details</button>
         </div>
       </Card>
     </li>
