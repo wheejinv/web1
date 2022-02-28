343e3d39098492f1ab0dd5987a6ec1509904adf0
wheejinv <wheejinv@naver.com>
2022-02-28 20:37:43 +0900

    React 완벽 가이드 - 섹션 23-2: Next.js 로 MeetUps 프로젝트 #5 add custom components - MeetupDetail

--- react-complete-guide/23-2-nextjs-meetups/pages/[meetupId]/index.js
+++ react-complete-guide/23-2-nextjs-meetups/pages/[meetupId]/index.js
@@ -1,6 +1,35 @@
+import {useRouter} from "next/router";
+import {Fragment} from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const DUMMY_MEETUPS = [
+	{
+		id: 'm1',
+		image: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbLoX5W%2Fbtqz6W4AmF0%2FrRSQrvEKfa1M8YBLH6KDIK%2Fimg.jpg',
+		title: 'A First Meetup',
+		address: 'Some address, 1234, city',
+		description: 'This is a first meetup'
+	},
+	{
+		id: 'm2',
+		image: 'https://t1.daumcdn.net/cfile/tistory/99A7B7365A9F7BA61B',
+		title: 'A Second Meetup',
+		address: 'nice address, 1234, city',
+		description: 'This is a second meetup'
+	}
+]
+
 function Index() {
+	const router = useRouter();
+
+	console.log(router.query.meetupId);
+
+	let meetupData = DUMMY_MEETUPS.find( item => item.id === router.query.meetupId);
+
 	return (
-		<div>meetupid</div>
+		<MeetupDetail
+			{...meetupData}
+		/>
 	);
 }
 
