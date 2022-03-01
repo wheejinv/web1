0f2770e443092dcba4967881d321578b7f4f52cd
wheejinv <wheejinv@naver.com>
2022-03-01 12:44:47 +0900

    React 완벽 가이드 - 섹션 23-2: Next.js 로 MeetUps 프로젝트 #11 sending http request to our API routes
    fetch, post, API, router

--- react-complete-guide/23-2-nextjs-meetups/pages/new-meetup/index.js
+++ react-complete-guide/23-2-nextjs-meetups/pages/new-meetup/index.js
@@ -1,11 +1,25 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import {useRouter} from "next/router";
 
 function NewMeetupPage() {
-const addMeetupHandler = enteredMeetupData => {
-	let {title, image, address, description} = enteredMeetupData;
+	const router = useRouter();
 
-	console.log(enteredMeetupData)
-}
+	const addMeetupHandler = async enteredMeetupData => {
+		console.log(enteredMeetupData)
+
+		const response = await fetch('/api/new-meetup', {
+			method: 'POST',
+			body: JSON.stringify(enteredMeetupData),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		const data = await response.json();
+
+		console.log(data);
+
+		router.push('/');
+	}
 
 
 	return (
