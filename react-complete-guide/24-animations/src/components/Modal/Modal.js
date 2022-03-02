c4d14154affd7c7ae6160cdcf197bb25be20d515
wheejinv <wheejinv@naver.com>
2022-03-02 19:57:20 +0900

    React 완벽 가이드 - 섹션 24: 리액트 앱 애니메이션 만들기. #6 Modal refactoring react-transition-group 에서 만든 Transition 컴포넌트를 사용
    https://reactcommunity.org/react-transition-group/transition

--- react-complete-guide/24-animations/src/components/Modal/Modal.js
+++ react-complete-guide/24-animations/src/components/Modal/Modal.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 import "./Modal.css";
+import {Transition} from "react-transition-group";
 
 const modal = props => {
-	const cssClasses = [
-		"Modal",
-		props.show ? "ModalOpen" : "ModalClosed"
-	];
+	const animationTiming = {
+		enter: 400,
+		exit: 1000,
+	};
 
 	return (
-		<div className={cssClasses.join(' ')}>
-			<h1>A Modal</h1>
-			<button className="Button" onClick={props.closed}>
-				Dismiss
-			</button>
-		</div>
+		<Fragment>
+			<Transition
+				in={props.show}
+				timeout={animationTiming}
+				mountOnEnter
+				unmountOnExit
+			>
+				{state => {
+					const cssClasses = [
+						"Modal",
+						state === 'entering'
+							? "ModalOpen"
+							: state === 'exiting' ? "ModalClosed" : null
+					];
+
+					return (
+						<div className={cssClasses.join(' ')}>
+							<h1>A Modal</h1>
+							<button className="Button" onClick={props.closed}>
+								Dismiss
+							</button>
+						</div>
+					);
+				}}
+			</Transition>
+		</Fragment>
 	);
 };
 
