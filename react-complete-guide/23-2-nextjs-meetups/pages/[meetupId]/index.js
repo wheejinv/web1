653fdf9971bd7e639fbc7a01d285bb3278bb9612
wheejinv <wheejinv@naver.com>
2022-03-02 13:45:02 +0900

    React 완벽 가이드 - 섹션 23-2: Next.js 로 MeetUps 프로젝트 #13 getting meetup detilas data
    몽고디비에서 _id 를 찾는 경우 ObjectId 함수 사용

--- react-complete-guide/23-2-nextjs-meetups/pages/[meetupId]/index.js
+++ react-complete-guide/23-2-nextjs-meetups/pages/[meetupId]/index.js
@@ -1,5 +1,6 @@
 import {useRouter} from "next/router";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
+import {MongoClient, ObjectId} from "mongodb";
 
 function Index(props) {
 	const router = useRouter();
@@ -19,15 +20,34 @@ function Index(props) {
 export async function getStaticProps(context) {
 	const meetupId = context.params.meetupId;
 
+	const env = process.env;
+	const url = `mongodb://${env.ID}:${env.PASS}@${env.URL}/${env.DBNAME}?authSource=admin`;
+
+	const client = await MongoClient.connect(url);
+	const db = client.db();
+
+	const meetupCollection = db.collection('meetups');
+
+	// _id 찾을 때 ObjectId 사용
+	const selectedMeetup = await meetupCollection.findOne({
+		_id: ObjectId(meetupId)
+	});
+
+	// console.log('selected', selectedMeetup)
+
+	const {title, image, address, description} = selectedMeetup;
+
+	client.close();
+
 	return {
 		props: {
 			meetupData: {
-				id: meetupId,
-				image: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbLoX5W%2Fbtqz6W4AmF0%2FrRSQrvEKfa1M8YBLH6KDIK%2Fimg.jpg',
-				title: 'A First Meetup',
-				address: 'Some address, 1234, city',
-				description: 'This is a first meetup',
-			}
+				id: selectedMeetup._id.toString(),
+				title,
+				image,
+				address,
+				description,
+			},
 		}
 	}
 }
@@ -36,6 +56,19 @@ export async function getStaticProps(context) {
 // API 등에서 패치하여 동적으로 배열을 만들어야 하겠지만 현재는 하드코딩.
 // https://nextjs.org/docs/basic-features/data-fetching/get-static-paths
 export async function getStaticPaths() {
+	const env = process.env;
+	const url = `mongodb://${env.ID}:${env.PASS}@${env.URL}/${env.DBNAME}?authSource=admin`;
+
+	const client = await MongoClient.connect(url);
+	const db = client.db();
+
+	const meetupCollection = db.collection('meetups');
+
+	// {_id: 1} -> ID만 포함하고 다른 필드 값은 포함하지 않는다
+	const meetups = await meetupCollection.find({}, {_id: 1}).toArray();
+
+	client.close();
+
 	return {
 		// NextJS 에게 paths 배열이 모든 지원되는 매개변수를 저장할지 아니면 일부만 저장할지 알려줌.
 		// 수백개의 페이지 중에 모든 것을 pre-generate 하지 않고 인기 있는 몇 개만 하고 싶을수도 있음.
@@ -43,18 +76,13 @@ export async function getStaticPaths() {
 		// false: paths 는 모든 meetupId 를 포함해야 함.
 		// true: 들어오는 요청에 관해서, 서버에서 meetupId로 동적으로 만듬.
 		fallback: false,
-		paths: [
-			{
+		paths: meetups.map( meetup => {
+			return {
 				params: {
-					meetupId: 'm1',
-				},
-			},
-			{
-				params: {
-					meetupId: 'm2',
-				},
-			},
-		]
+					meetupId: meetup._id.toString(),
+				}
+			}
+		}),
 	}
 }
 
