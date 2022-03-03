fbd081cf608c4ed0c7449b48ba6ec8a262ed8327
wheejinv <wheejinv@naver.com>
2022-03-03 19:52:19 +0900

    React 완벽 가이드 - 섹션 25: redux -> react hook 으로 바꾸기 #2 custom hook 으로 redux 대체

--- react-complete-guide/25-replacing-redux/src/containers/Products.js
+++ react-complete-guide/25-replacing-redux/src/containers/Products.js
@@ -3,9 +3,11 @@ import { useSelector } from 'react-redux';
 
 import ProductItem from '../components/Products/ProductItem';
 import './Products.css';
+import {useStore} from "../hooks-store/store";
 
 const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+	const state = useStore()[0];
+  const productList = state.products;
   return (
     <ul className="products-list">
       {productList.map(prod => (
