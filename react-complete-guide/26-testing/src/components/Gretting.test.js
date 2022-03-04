eb7ffaa43f3222df88d698508678e303292d1351
wheejinv <wheejinv@naver.com>
2022-03-04 16:10:24 +0900

    React 완벽 가이드 - 섹션 26: 리액트 앱 테스트(단위 테스트) #3 첫번째 테스트 작성

---
+++ react-complete-guide/26-testing/src/components/Gretting.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {render} from "@testing-library/react";
+import Greeting from "./Greeting";
+
+
+
+test('renders Hello World as a text', () => {
+	// Three A
+	// Arrange: 테스트 데이터 설정
+	// Act: 테스트를 실행
+	// Assert: 아웃풋을 검토
+
+	// Arrange
+	render(<Greeting/>);
+
+	// Act
+	// ... nothing
+
+	// Assert
+})
