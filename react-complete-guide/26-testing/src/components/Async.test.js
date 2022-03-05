e6d62b70eaed7b075cbbbe955d78ed3135d0b738
wheejinv <wheejinv@naver.com>
2022-03-05 18:30:58 +0900

    React 완벽 가이드 - 섹션 26: 리액트 앱 테스트(단위 테스트) #8 fetch API mocking
    mockResolvedValueOnce

--- react-complete-guide/26-testing/src/components/Async.test.js
+++ react-complete-guide/26-testing/src/components/Async.test.js
@@ -2,11 +2,14 @@ import React from 'react';
 import {render, screen} from "@testing-library/react";
 import Async from "./Async";
 
-// https://stackoverflow.com/questions/48433783/referenceerror-fetch-is-not-defined
-global.fetch = require("node-fetch");
-
 describe('Async component', () => {
 	test('renders posts if request succeeds', async () => {
+		window.fetch = jest.fn();
+		window.fetch.mockResolvedValueOnce({
+			json: async () => ([
+				{"id": 1, "title": "first post"}
+			])
+		})
 		render(<Async/>)
 
 		// timeout default 1초
