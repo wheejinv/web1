5fe6575dadaec271df6dc54e622c5dbade877c52
wheejinv <wheejinv@naver.com>
2022-03-05 16:24:45 +0900

    React 완벽 가이드 - 섹션 26: 리액트 앱 테스트(단위 테스트) #5 describe 테스트 그룹화

--- react-complete-guide/26-testing/src/components/Gretting.test.js
+++ react-complete-guide/26-testing/src/components/Gretting.test.js
@@ -2,21 +2,22 @@ import React from 'react';
 import {render, screen} from "@testing-library/react";
 import Greeting from "./Greeting";
 
+// Test Suites 그룹을 위한 문법
+describe('Greeting component', () => {
+	test('renders Hello World as a text', () => {
+		// 테스트의 Three A
+		// Arrange: 테스트 데이터 설정
+		// Act: 테스트를 실행
+		// Assert: 아웃풋을 검토
 
+		// Arrange
+		render(<Greeting/>);
 
-test('renders Hello World as a text', () => {
-	// 테스트의 Three A
-	// Arrange: 테스트 데이터 설정
-	// Act: 테스트를 실행
-	// Assert: 아웃풋을 검토
+		// Act
+		// ... nothing
 
-	// Arrange
-	render(<Greeting/>);
-
-	// Act
-	// ... nothing
-
-	// Assert
-	const helloWorldElement = screen.getByText('Hello World!');
-	expect(helloWorldElement).toBeInTheDocument();
+		// Assert
+		const helloWorldElement = screen.getByText('Hello World!');
+		expect(helloWorldElement).toBeInTheDocument();
+	})
 })
