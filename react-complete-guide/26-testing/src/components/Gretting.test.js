3f4f91be1ef762388edb251271f338b65d881b8d
wheejinv <wheejinv@naver.com>
2022-03-05 17:15:23 +0900

    React 완벽 가이드 - 섹션 26: 리액트 앱 테스트(단위 테스트) #6 사용자 상호 작용 및 State 테스트하기

--- react-complete-guide/26-testing/src/components/Gretting.test.js
+++ react-complete-guide/26-testing/src/components/Gretting.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render, screen} from "@testing-library/react";
 import Greeting from "./Greeting";
+import userEvent from '@testing-library/user-event';
 
 // Test Suites 그룹을 위한 문법
 describe('Greeting component', () => {
@@ -20,4 +21,31 @@ describe('Greeting component', () => {
 		const helloWorldElement = screen.getByText('Hello World!');
 		expect(helloWorldElement).toBeInTheDocument();
 	})
+
+	// 버튼이 눌렀을 때 상태가 변하는지 테스트
+	test('on button clicked', () => {
+		// Arrange
+		render(<Greeting/>);
+
+		// Act
+		const buttonElement = screen.getByRole('button');
+		userEvent.click(buttonElement);
+
+		// Assert
+		const changedElement = screen.getByText('Changed!');
+		expect(changedElement).toBeInTheDocument();
+	});
+
+	test('does not render "good to see you" if the button was clicked', () => {
+		// Arrange
+		render(<Greeting />);
+
+		// Act
+		const buttonElement = screen.getByRole('button');
+		userEvent.click(buttonElement)
+
+		// Assert
+		const outputElement = screen.queryByText('good to see you', { exact: false });
+		expect(outputElement).toBeNull();
+	});
 })
