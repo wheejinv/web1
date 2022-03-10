d362a45b7259002949a2ca34a6b50357cee81a29
wheejinv <wheejinv@naver.com>
2022-03-11 05:20:49 +0900

    [coding-test] 쇼핑 SPA 솔루션 #7 장바구니 상품 불러오기 & 주문 시 저장데이터 삭제

--- coding-test/programmers/202109-shopping-spa/solution/pages/CartPage.js
+++ coding-test/programmers/202109-shopping-spa/solution/pages/CartPage.js
@@ -1,12 +1,62 @@
+import {routeChange} from "../router.js";
+import {getItem} from "../storage.js";
+import {mockRequest} from "../api.js";
+import Cart from "../components/Cart.js";
+
 function CartPage({ $target }) {
 	const $page = document.createElement('div')
 	$page.className = 'CartPage'
 
 	$page.innerHTML = '<h1>장바구니</h1>'
 
-	this.render = () => {
-		$target.appendChild($page)
+	const cartData = getItem('products_cart', [])
+	this.state = {
+		products: null
 	}
+
+	let cartComponent = null
+
+	this.setState = (nextState) => {
+		this.state = nextState
+		this.render()
+	}
+
+	this.render = () => {
+		if (cartData.length === 0) {
+			alert('장바구니가 비어있습니다.')
+			routeChange('/')
+		} else {
+			$target.appendChild($page)
+
+			// Cart 컴포넌트 생성
+			if (this.state.products && !cartComponent) {
+				cartComponent = new Cart({
+					$target: $page,
+					initialState: this.state.products
+				})
+			}
+		}
+	}
+
+	this.fetchProducts = async () => {
+		const products = await Promise.all(cartData.map(async (cartItem) => {
+			const product = await mockRequest(`/products/${cartItem.productId}`)
+			const selectedOption = product.productOptions.find(option => option.id === cartItem.optionId)
+
+			return {
+				imageUrl: product.imageUrl,
+				productName: product.name,
+				quantity: cartItem.quantity,
+				productPrice: product.price,
+				optionName: selectedOption.name,
+				optionPrice: selectedOption.price
+			}
+		}))
+
+		this.setState({ products })
+	}
+
+	this.fetchProducts()
 }
 
 export default CartPage;
