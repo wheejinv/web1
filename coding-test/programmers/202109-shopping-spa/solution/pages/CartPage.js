003e936847f4f39d17ff3cbba5e76e34c54b80f7
wheejinv <wheejinv@naver.com>
2022-03-11 02:26:20 +0900

    [coding-test] 쇼핑 SPA 솔루션 #1 라우트 처리
    https://prgms.tistory.com/113

---
+++ coding-test/programmers/202109-shopping-spa/solution/pages/CartPage.js
@@ -0,0 +1,12 @@
+function CartPage({ $target }) {
+	const $page = document.createElement('div')
+	$page.className = 'CartPage'
+
+	$page.innerHTML = '<h1>장바구니</h1>'
+
+	this.render = () => {
+		$target.appendChild($page)
+	}
+}
+
+export default CartPage;
---
+++ coding-test/programmers/202109-shopping-spa/solution/pages/ProductListPage.js
@@ -0,0 +1,12 @@
+function ProductListPage({$target}) {
+	const $page = document.createElement('div')
+	$page.className = 'ProductListPage'
+
+	$page.innerHTML = '<h1>상품 목록</h1>'
+
+	this.render = () => {
+		$target.appendChild($page)
+	}
+}
+
+export default ProductListPage;
