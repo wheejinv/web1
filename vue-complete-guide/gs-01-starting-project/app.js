d8ba98d9c5d5eef268c06e6dc6cf9e820d94b5d1
wheejinv <wheejinv@naver.com>
2022-04-28 06:30:13 +0900

    [Vue-Complete-Guide] 1. Getting Started #3 Re-building the App with Vue

--- vue-complete-guide/gs-01-starting-project/app.js
+++ vue-complete-guide/gs-01-starting-project/app.js
@@ -1,14 +1,29 @@
-let goal = document.getElementById('goal');
+Vue.createApp({
+	data() {
+		return {
+			goals: [],
+			enteredValue: ''
+		};
+	},
+	methods: {
+		addGoal() {
+			this.goals.push(this.enteredValue);
+			this.enteredValue = '';
+		}
+	}
+}).mount('#app');
 
-const buttonElement = document.querySelector('button');
-const inputElement = document.querySelector('input');
-const listElement = document.querySelector('ul');
-
-buttonElement.addEventListener('click', e => {
-	const enteredValue = inputElement.value;
-	const listItemElement = document.createElement('li');
-	listItemElement.textContent = enteredValue;
-	listElement.appendChild(listItemElement);
-	inputElement.value = '';
-
-})
+// let goal = document.getElementById('goal');
+//
+// const buttonElement = document.querySelector('button');
+// const inputElement = document.querySelector('input');
+// const listElement = document.querySelector('ul');
+//
+// buttonElement.addEventListener('click', e => {
+// 	const enteredValue = inputElement.value;
+// 	const listItemElement = document.createElement('li');
+// 	listItemElement.textContent = enteredValue;
+// 	listElement.appendChild(listItemElement);
+// 	inputElement.value = '';
+//
+// })
