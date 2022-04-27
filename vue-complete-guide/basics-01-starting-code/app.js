faf4293efb3bba7a24325dd685d70adb03f01159
wheejinv <wheejinv@naver.com>
2022-04-28 08:27:16 +0900

    [Vue-Complete-Guide] 2. Basics & Core Concepts - DOM Interaction with Vue #2 data binding, v-bind

---
+++ vue-complete-guide/basics-01-starting-code/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			courseGoal: 'Finish the course and learn Vue!',
+      vueLink: 'https://vuejs.org'
+		}
+	}
+});
+
+app.mount('#user-goal');
+
