f294181245110b93a5d9c0c72ae29550d3295592
wheejinv <wheejinv@naver.com>
2022-04-28 08:50:08 +0900

    [Vue-Complete-Guide] 2. Basics & Core Concepts - DOM Interaction with Vue #4 v-html: 변수를 html 로 사용하고 싶을 때 사용 / 기본적으로 string 으로 변수들을 취급하며 이건 크로스 사이트 스크립팅 공격 방지에 유용하기 때문.

--- vue-complete-guide/02_basics-01-starting-code/app.js
+++ vue-complete-guide/02_basics-01-starting-code/app.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
   data() {
     return {
       courseGoalA: 'Finish the course and learn Vue!',
-      courseGoalB: 'Master Vue and build amazing apps!',
+      courseGoalB: '<h3>Master Vue and build amazing apps!</h3>',
       vueLink: 'https://vuejs.org'
     }
   },
