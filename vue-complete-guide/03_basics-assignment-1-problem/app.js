cedc41f8eac446d7c504480f0aab41b2c79cd143
wheejinv <wheejinv@naver.com>
2022-04-28 09:20:13 +0900

    [Vue-Complete-Guide] 2. Basics & Core Concepts - DOM Interaction with Vue #6 과제 complete

---
+++ vue-complete-guide/03_basics-assignment-1-problem/app.js
@@ -0,0 +1,12 @@
+const vue = Vue.createApp({
+  data() {
+    return {
+      name: 'HeeJin Ha',
+      age: 30,
+      imageUrl: 'https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip',
+    }
+  },
+  methods() {
+
+  }
+}).mount('body')
