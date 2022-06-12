28a9e8e6f2889454a03d952ae313955c7ba41607
wheejinv <wheejinv@naver.com>
2022-06-13 00:50:58 +0900

    [Nuxt] nuxtjs 에서 sass 전역 변수 사용
    https://dev.to/paramo/using-sass-global-variables-in-nuxt-js-j0k

---
+++ nuxtjs/nuxt-static-test-em-shop/pages/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="main">메인 페이지</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.main {
+  color: $color5;
+}
+
+</style>
