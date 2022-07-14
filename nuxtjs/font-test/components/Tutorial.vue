3eb46220aa2dfe7b8e0172f80e5d40d6ba909deb
wheejinv <wheejinv@naver.com>
2022-07-15 00:13:41 +0900

    init

---
+++ nuxtjs/font-test/components/Tutorial.vue
@@ -0,0 +1,38 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="main">
+    hello world from client!
+
+    <div class="from-server">
+      {{response}}
+    </div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'IndexPage',
+  async mounted() {
+    const {data} = await axios.get('http://localhost:3001/');
+    this.response = data;
+  },
+  data() {
+    return {
+      response: 'hello default'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.main {
+  color: $test-css;
+
+  .from-server {
+    color: black;
+  }
+}
+</style>
